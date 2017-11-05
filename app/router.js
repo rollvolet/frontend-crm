8e3ed75747c225fcd0c8f8f70de83018106b28c2
Erika Pauwels <erika.pauwels@gmail.com>
2017-11-05 17:38:06 +0100

    Add main side menu

--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
-  this.route('main', { path: '/' });
+  this.route('main', { path: '/' }, function() {});
 });
 
 export default Router;
