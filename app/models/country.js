5237c56e729315812dd5af8e416c7380e43c5f88
Erika Pauwels <erika.pauwels@gmail.com>
2017-11-12 21:21:11 +0100

    Add telephone prefix to country model

--- app/models/country.js
+++ app/models/country.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   code: DS.attr(),
   name: DS.attr(),
+  telephonePrefix: DS.attr(),
   telephones: DS.hasMany('telephone'),
   contacts: DS.hasMany('contact'),
   buildings: DS.hasMany('building'),
