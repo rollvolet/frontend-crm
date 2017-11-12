83c467b3cb37278d0d4f03c23e93f14ff85951e4
Erika Pauwels <erika.pauwels@gmail.com>
2017-11-12 21:21:37 +0100

    Don't include telephones by default on customers

--- app/routes/main/customers/edit.js
+++ app/routes/main/customers/edit.js
@@ -3,7 +3,7 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model(params) {
     return this.get('store').findRecord('customer', params.customer_id, {
-      include: 'postal-code,language,country,honorific-prefix,telephones'
+      include: 'postal-code,language,country,honorific-prefix'
     });
   }
 });
