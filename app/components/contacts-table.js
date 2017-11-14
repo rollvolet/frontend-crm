4bf439b30ce9aaae3c070554b18ed1da825b35aa
Erika Pauwels <erika.pauwels@gmail.com>
2017-11-14 23:07:59 +0100

    Add pagination to contacts table

--- app/components/contacts-table.js
+++ app/components/contacts-table.js
@@ -3,12 +3,12 @@ import { computed } from '@ember/object';
 
 export default Component.extend({
   page: 0,
-  size: 25,
+  size: 10,
   contacts: computed('customer', 'page', 'size', function() {
     return this.get('customer').query('contacts', {
       page: {
         size: this.get('size'),
-        page: this.get('page')
+        number: this.get('page')
       }
     });
   })
--- app/serializers/application.js
+++ app/serializers/application.js
@@ -2,5 +2,20 @@ import DS from 'ember-data';
 import DataTableSerializerMixin from 'ember-data-table/mixins/serializer';
 
 export default DS.JSONAPISerializer.extend(DataTableSerializerMixin, {
+  /**
+      Parse the links in the JSONAPI response and convert to a meta-object
+   */
+  normalizeArrayResponse(store, primaryModelClass, payload, id, requestType) {
+    const result = this._super(...arguments);
+    result.meta = result.meta || {};
 
+    if (payload.links) {
+      result.meta.pagination = this.createPageMeta(payload.links);
+    }
+    if (payload.meta) {
+      result.meta.count = payload.meta.count;
+    }
+
+    return result;
+  }
 });
