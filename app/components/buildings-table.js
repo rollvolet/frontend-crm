c54081d2b68032cf968c2918011b89d102f76a75
Erika Pauwels <erika.pauwels@gmail.com>
2017-12-09 18:28:29 +0100

    Add search to buildings table

--- app/components/buildings-table.js
+++ app/components/buildings-table.js
@@ -1,18 +1,54 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
+import { observer } from '@ember/object';
+import { task, timeout } from 'ember-concurrency';
 
 export default Component.extend({
+  classNames: ['buildings-table'],
+  init() {
+    this._super(...arguments);
+    this.get('search').perform();
+  },
   page: 0,
   size: 10,
   sort: 'name',
-  buildings: computed('customer', 'page', 'size', 'sort', function() {
-    return this.get('customer').query('buildings', {
+  dataTableParamChanged: observer('page', 'size', 'sort', function() {
+    this.get('search').perform();
+  }),
+  debouncedSearch: task(function * () {
+    yield timeout(500);
+    this.set('page', 0);
+    yield this.get('search').perform();
+  }).restartable(),
+  search: task(function * () {
+    const buildings = yield this.get('customer').query('buildings', {
       page: {
         size: this.get('size'),
         number: this.get('page')
       },
       sort: this.get('sort'),
-      include: 'country,language,honorific-prefix'
+      include: 'country,language,honorific-prefix',
+      filter: {
+        number: this.get('number'),
+        name: this.get('name'),
+        'postal-code': this.get('postalCode'),
+        city: this.get('city'),
+        street: this.get('street')
+      }
     });
-  })
+    this.set('buildings', buildings);
+  }),
+  actions: {
+    setFilter(key, value) {
+      this.set(key, value);
+      this.get('debouncedSearch').perform();
+    },
+    resetFilters() {
+      this.set('number', undefined);
+      this.set('name', undefined);
+      this.set('postalCode', undefined);
+      this.set('city', undefined);
+      this.set('street', undefined);
+      this.get('search').perform();
+    }
+  }
 });
