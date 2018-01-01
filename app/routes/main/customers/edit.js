b55809aa0ce69bd6d5061e9164f57d1db80885d1
Erika Pauwels <erika.pauwels@gmail.com>
2018-01-01 12:29:09 +0100

    Remove include telephones from customer's edit route

--- app/routes/main/customers/edit.js
+++ app/routes/main/customers/edit.js
@@ -3,7 +3,9 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model(params) {
     return this.get('store').findRecord('customer', params.customer_id, {
-      include: 'language,country,honorific-prefix,tags,telephones'
+      // don't include telephones here. Telephones need to be retrieved in a separate request
+      // so we can include the telephone types in them
+      include: 'language,country,honorific-prefix,tags'
     });
   }
 });
