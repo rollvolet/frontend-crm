4b9421c394ce6b8d3706a6cd43a3b159326665b4
Erika Pauwels <erika.pauwels@gmail.com>
2018-01-11 13:58:28 +0100

    Add authorization to backend calls

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
 import HasManyQuery from 'ember-data-has-many-query';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default DS.JSONAPIAdapter.extend(HasManyQuery.RESTAdapterMixin, {
+export default DS.JSONAPIAdapter.extend(HasManyQuery.RESTAdapterMixin, DataAdapterMixin, {
+  authorizer: 'authorizer:oauth2'
 });
