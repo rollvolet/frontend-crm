a467032c57241915e2529038077d435701dcf8f9
Erika Pauwels <erika.pauwels@gmail.com>
2018-03-01 13:54:51 +0100

    Add deposits and deposit invoices to order detail

--- app/controllers/main/case/order/index.js
+++ app/controllers/main/case/order/index.js
@@ -1,11 +1,22 @@
 import Controller from '@ember/controller';
-import ReduceFunctions from '../../../../mixins/reduce-functions';
+import { mapBy } from 'ember-awesome-macros/array';
+import { add, product, quotient, subtract, sum } from 'ember-awesome-macros';
+import raw from 'ember-macro-helpers/raw';
 
-export default Controller.extend(ReduceFunctions, {
-  showDeposits: false,
+export default Controller.extend({
+  showDepositsDialog: false,
+  depositsAmount: sum(mapBy('model.deposits', raw('amount'))),
+  depositInvoicesAmount: sum(mapBy('model.depositInvoices', raw('arithmeticAmount'))),
   actions: {
-    toggleShowDeposits() {
-      this.toggleProperty('showDeposits');
+    closeDepositsDialog() {
+      this.set('showDepositsDialog', false);
+    },
+    openDepositsDialog() {
+      this.set('showDepositsDialog', true);
+    },
+    goToDepositInvoices() {
+      const order = this.get('model');
+      this.transitionToRoute('main.case.order.deposit-invoices', order);
     }
   }
 });
--- app/routes/main/case/order/index.js
+++ app/routes/main/case/order/index.js
@@ -1,5 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-
+  resetController(controller) {
+    controller.set('showDepositsDialog', false);
+  }
 });
