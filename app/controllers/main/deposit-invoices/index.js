40bbef5f45cb4970b0f9d0bd30a06d97fa8c38d6
Erika Pauwels <erika.pauwels@gmail.com>
2018-03-26 18:13:55 +0200

    Add data table for deposit invoices incl. sort/filtering

---
+++ app/controllers/main/deposit-invoices/index.js
@@ -0,0 +1,51 @@
+import Controller from '@ember/controller';
+import DefaultQueryParams from 'ember-data-table/mixins/default-query-params';
+import DebouncedSearch from '../../../mixins/debounced-search-task';
+import { oneWay } from '@ember/object/computed';
+
+export default Controller.extend(DefaultQueryParams, DebouncedSearch, {
+  size: 25,
+  sort: '-number',
+
+  numberFilter: oneWay('number'),
+  referenceFilter: oneWay('reference'),
+  offerNumberFilter: oneWay('offerNumber'),
+  cNameFilter: oneWay('cName'),
+  cPostalCodeFilter: oneWay('cPostalCode'),
+  cCityFilter: oneWay('cCity'),
+  cStreetFilter: oneWay('cStreet'),
+  cTelephoneFilter: oneWay('cTelephone'),
+  bNameFilter: oneWay('bName'),
+  bPostalCodeFilter: oneWay('bPostalCode'),
+  bCityFilter: oneWay('bCity'),
+  bStreetFilter: oneWay('bStreet'),
+
+  actions: {
+    clickRow(row) {
+      const customerId = row.get('customer.id');
+      const orderId = row.get('order.id');
+      this.transitionToRoute('main.case.order.deposit-invoices', customerId, orderId);
+    },
+    setFilter(key, value) {
+      this.set(`${key}Filter`, value);
+      this.get('debounceFilter').perform(key, value);
+    },
+    resetFilters() {
+      [
+        'numberFilter', 'number',
+        'referenceFilter', 'reference',
+        'offerNumberFilter', 'offerNumber',
+        'cNameFilter', 'cName',
+        'cPostalCodeFilter', 'cPostalCode',
+        'cCityFilter', 'cCity',
+        'cStreetFilter', 'cStreet',
+        'cTelephoneFilter', 'cTelephone',
+        'bNameFilter', 'bName',
+        'bPostalCodeFilter', 'bPostalCode',
+        'bCityFilter', 'bCity',
+        'bStreetFilter', 'bStreet'
+      ].forEach(x => this.set(x, undefined));
+    }
+  }
+
+});
---
+++ app/routes/main/deposit-invoices/index.js
@@ -0,0 +1,51 @@
+import Route from '@ember/routing/route';
+import DataTableRouteMixin from 'ember-data-table/mixins/route';
+
+export default Route.extend(DataTableRouteMixin, {
+  modelName: 'deposit-invoice',
+  queryParams: {
+    page: { refreshModel: true },
+    size: { refreshModel: true },
+    sort: { refreshModel: true },
+    // filter params
+    number: { refreshModel: true },
+    offerNumber: { refreshModel: true },
+    reference: { refreshModel: true },
+    cName: { refreshModel: true },
+    cPostalCode: { refreshModel: true },
+    cCity: { refreshModel: true },
+    cStreet: { refreshModel: true },
+    cTelephone: { refreshModel: true },
+    bName: { refreshModel: true },
+    bPostalCode: { refreshModel: true },
+    bCity: { refreshModel: true },
+    bStreet: { refreshModel: true }
+  },
+  mergeQueryOptions(params) {
+    return {
+      // Building and contact must already be included
+      // such that correct values can be set in the case controller when opening the detail
+      include: 'customer,customer.honorific-prefix,order,building,contact',
+      filter: {
+        number: params.number,
+        reference: params.reference,
+        offer: {
+          number: params.offerNumber
+        },
+        customer: {
+          name: params.cName,
+          'postal-code': params.cPostalCode,
+          city: params.cCity,
+          street: params.cStreet,
+          telephone: params.cTelephone
+        },
+        building: {
+          name: params.bName,
+          'postal-code': params.bPostalCode,
+          city: params.bCity,
+          street: params.bStreet
+        }
+      }
+    };
+  }
+});
