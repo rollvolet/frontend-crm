d468f2cfa173579085c3b27cc8dee8b4422da73b
Erika Pauwels <erika.pauwels@gmail.com>
2018-05-13 20:43:28 +0200

    Remove updated date from contact/building model

--- app/models/contact.js
+++ app/models/contact.js
@@ -21,9 +21,6 @@ export default DS.Model.extend({
   created: DS.attr('date', {
     defaultValue() { return new Date(); }
   }),
-  updated: DS.attr('date', {
-    defaultValue() { return new Date(); }
-  }),
   customer: DS.belongsTo('customer'),
   country: DS.belongsTo('country'),
   language: DS.belongsTo('language'),
