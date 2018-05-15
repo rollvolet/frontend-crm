f38860fa74ddd1e957a008b73169e89cb0731772
Erika Pauwels <erika.pauwels@gmail.com>
2018-05-15 17:00:10 +0200

    Remove working hours amount from invoice detail

--- app/controllers/main/case/invoice.js
+++ app/controllers/main/case/invoice.js
@@ -1,11 +1,7 @@
 import Controller from '@ember/controller';
-import { mapBy } from 'ember-awesome-macros/array';
-import { sum } from 'ember-awesome-macros';
-import raw from 'ember-macro-helpers/raw';
 
 export default Controller.extend({
   showWorkingHoursDialog: false,
-  workingHoursAmount: sum(mapBy('model.workingHours', raw('amount'))),
   actions: {
     closeWorkingHoursDialog() {
       this.set('showWorkingHoursDialog', false);
--- app/models/working-hour.js
+++ app/models/working-hour.js
@@ -2,7 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   date: DS.attr('date'),
-  amount: DS.attr(),
   invoice: DS.belongsTo('invoice'),
   employee: DS.belongsTo('employee')
 });
