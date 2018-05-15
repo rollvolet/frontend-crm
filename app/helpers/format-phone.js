cd1dd58d7862316d4027a11336ebba36d2c7b634
Erika Pauwels <erika.pauwels@gmail.com>
2018-05-15 17:04:15 +0200

    Update formatting of phone area number

--- app/helpers/format-phone.js
+++ app/helpers/format-phone.js
@@ -4,6 +4,10 @@ export function formatPhone([prefix, area, number]) {
   if (prefix && prefix.startsWith("00"))
     prefix = prefix.replace("00", "+"); // replaces only the 1st occurence
 
+  if (area && area.length > 0) {
+    area = `(${area.substr(0, 1)})${area.substr(1)}`;
+  }
+
   if (number) {
     if (number.length == 6)
       number = `${number.substr(0,2)} ${number.substr(2,2)} ${number.substr(4)}`;
