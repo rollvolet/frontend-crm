33ac863ed6b1c21779bae42d42097e4a466c7ab3
Erika Pauwels <erika.pauwels@gmail.com>
2018-05-23 16:59:48 +0200

    Add validation before updating a request

--- app/controllers/main/case/request/edit.js
+++ app/controllers/main/case/request/edit.js
@@ -1,13 +1,15 @@
 import Controller from '@ember/controller';
 import { task } from 'ember-concurrency';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+  validation: service(),
+
   queryParams: ['editMode'],
   editMode: false,
 
   isValid() {
-    // TODO implement
-    return true;
+    return this.validation.required(this.model.get('requestDate'), 'Datum');
   },
 
   save: task(function * () {
