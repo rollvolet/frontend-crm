65150ff1c362c4f40eaacd459ff16b30afd74504
Erika Pauwels <erika.pauwels@gmail.com>
2018-07-12 16:27:31 +0200

    Support non-Belgian VAT numbers

--- app/helpers/format-vat-number.js
+++ app/helpers/format-vat-number.js
@@ -2,11 +2,16 @@ import { helper } from '@ember/component/helper';
 
 export function formatVatNumber([vatNumber]) {
   if (vatNumber) {
-    const prefix = vatNumber.substr(0, 2);
+    const country = vatNumber.substr(0, 2);
     const number = vatNumber.substr(2);
-    vatNumber = `${prefix} ${number.substr(0,4)}.${number.substr(4,3)}.${number.substr(7)}`;
+    if (country.toUpperCase() == 'BE') {
+      if (number.length == 9)
+        return `${country} 0${number.substr(0,3)}.${number.substr(3,3)}.${number.substr(6)}`;
+      else if (number.length > 9)
+        return `${country} ${number.substr(0,4)}.${number.substr(4,3)}.${number.substr(7)}`;
+    }
+    return `${country} ${number}`;
   }
-
   return vatNumber;
 }
 
