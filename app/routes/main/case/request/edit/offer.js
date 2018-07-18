da696ae1e656bd9cd43f96bd4d4749064470e805
Erika Pauwels <erika.pauwels@gmail.com>
2018-07-18 13:46:49 +0200

    Disable editing of contact/building in offer form

--- app/routes/main/case/request/edit/offer.js
+++ app/routes/main/case/request/edit/offer.js
@@ -1,14 +1,17 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  model() {
+  async model() {
     const customer = this.modelFor('main.case');
     const request = this.modelFor('main.case.request.edit');
+    const contact = await request.get('contact');
+    const building = await request.get('building');
     const offer = this.store.createRecord('offer', {
       offerDate: new Date(),
-      customer: customer,
-      request: request
-      // TODO add contact/building from request?
+      customer,
+      request,
+      contact,
+      building
     });
 
     return offer.save();
