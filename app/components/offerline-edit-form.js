aaeccc13dda48c6109dbac3ac8ade7b5a8908322
Erika Pauwels <erika.pauwels@gmail.com>
2018-08-07 17:26:49 +0200

    Add subscript and superscript actions to pell-editor

--- app/components/offerline-edit-form.js
+++ app/components/offerline-edit-form.js
@@ -1,16 +1,35 @@
 import Component from '@ember/component';
 import { task, timeout } from 'ember-concurrency';
 import DecimalInputFormatting from '../mixins/decimal-input-formatting';
+import { exec } from 'pell';
 
 export default Component.extend(DecimalInputFormatting, {
   classNames: ['layout-row', 'layout-align-start-center'],
 
   model: null,
   onDelete: null,
+  pellOptions: null,
 
   init() {
     this._super(...arguments);
     this.initDecimalInput('amount');
+    this.set('pellOptions', {
+      actions: [
+        'bold', 'underline', 'italic', 'strikethrough', 'olist', 'ulist',
+        {
+          name: 'subscript',
+          icon: 'x<sub>2</sub>',
+          title: 'Subscript',
+          result: () => exec('subscript')
+        },
+        {
+          name: 'superscript',
+          icon: 'x<sup>2</sup>',
+          title: 'Superscript',
+          result: () => exec('superscript')
+        }
+      ]
+    });
   },
 
   save: task(function * () {
