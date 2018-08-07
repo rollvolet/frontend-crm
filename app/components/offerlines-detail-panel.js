009e606cc914d1b080a75f2d83b6a11498818eb9
Erika Pauwels <erika.pauwels@gmail.com>
2018-08-07 17:44:41 +0200

    Freeze offer when edit is disabled
    Disable offer PDF generation and deletion of offerlines when edit is disabled

--- app/components/offerlines-detail-panel.js
+++ app/components/offerlines-detail-panel.js
@@ -1,13 +1,5 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  model: null,
-  onDelete: null,
-  isDisabledEdit: false,
-
-  actions: {
-    delete(offerline) {
-      this.onDelete(offerline);
-    }
-  }
+  model: null
 });
