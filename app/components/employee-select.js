90c98d37b99fc6fded414580dda27bc46f779aa6
Erika Pauwels <erika.pauwels@gmail.com>
2018-09-13 14:23:53 +0200

    Display all employees in dropdown on request page

--- app/components/employee-select.js
+++ app/components/employee-select.js
@@ -1,14 +1,26 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { sort } from 'ember-awesome-macros/array';
+
+const groupByFunction = function(arr) {
+  return arr.reduce(function(grouped, e) {
+    const group = e.function ? e.function[0] : '';
+    (grouped[group] = grouped[group] || []).push(e);
+    return grouped;
+  }, {});
+};
 
 export default Component.extend({
   store: service(),
-  options: computed('onlyActive', 'type', 'function', function() {
+  employees: computed('onlyActive', 'onlyWithFunction', 'type', 'function', function() {
     let employees = this.store.peekAll('employee');
 
     if (this.onlyActive)
       employees = employees.filter(e => e.active);
+
+    if (this.onlyWithFunction)
+      employees = employees.filter(e => e.function);
 
     if (this.type != null)
       employees = employees.filter(e => e.type == this.type);
@@ -18,10 +30,12 @@ export default Component.extend({
 
     return employees;
   }),
+  options: sort('employees', ['function:asc']),
   label: 'Werknemer',
   type: null,
   function: null,
   onlyActive: true,
+  onlyWithFunction: true,
   value: null,
   onSelectionChange: null
 });
