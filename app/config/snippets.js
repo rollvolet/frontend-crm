32e78ef1ebb5a6dac10d98585849e7d6ecfaeeff
Erika Pauwels <erika.pauwels@gmail.com>
2018-10-03 15:24:12 +0200

    Update offer document intro/outro snippets

--- app/config/snippets.js
+++ app/config/snippets.js
@@ -1,10 +1,10 @@
 export default {
   'NED': {
-    offerDocumentIntro: '<div>Ingevolge uw verzoek hebben wij de eer U offerte te doen voor het leveren en plaatsen van:</div>',
+    offerDocumentIntro: '<p class="aanhef">Geachte Heer, Mevrouw,</p><div>Ingevolge uw verzoek hebben wij de eer U offerte te doen voor het leveren en plaatsen van:</div>',
     offerDocumentOutro: '<div>Geldigheidsduur offerte: 1 maand, daarna vrijblijvend.</div><div>Betaling: 30% bij bestelling, saldo na plaatsing.</div>'
   },
   'FRA': {
-    offerDocumentIntro: 'French intro text for offer',
-    offerDocumentOutro: 'French outro text for offer'
+    offerDocumentIntro: `<p class="aanhef">Monsieur, Madame,</p><div>Suite à votre demande, nous avons l'avantage de vous faire offre pour fourniture et placement de:</div>`,
+    offerDocumentOutro: '<div>Option : 1 mois, après sans engagement</div><div>Payement : 30% à la commande, solde après placement.</div>'
   }
 };
