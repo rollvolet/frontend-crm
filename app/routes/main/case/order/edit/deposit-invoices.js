f7921f84401f25aeaf5cd1b73da1798eb85f535e
Erika Pauwels <erika.pauwels@gmail.com>
2018-10-13 19:09:45 +0200

    Fix model of the deposit invoices route of an order

--- app/routes/main/case/order/edit/deposit-invoices.js
+++ app/routes/main/case/order/edit/deposit-invoices.js
@@ -3,7 +3,7 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model() {
     const order = this.modelFor('main.case.order.edit');
-    return order.query('depositInvoices', {
+    return order.query('depositInvoices', { // TODO replace with query on depositInvoices filtered by orderId
       sort: '-number',
       page: {
         size: 1000 // we don't expect more than 1000 deposit invoices for 1 order
@@ -11,10 +11,13 @@ export default Route.extend({
       include: 'building,contact,vat-rate'
     });
   },
-  setupController(controller) {
+  setupController(controller, model) {
+    this._super(controller, model);
+
     const customer = this.modelFor('main.case');
-    const order = this.modelFor('main.case.order.edit');
     controller.set('customer', customer);
+
+    const order = this.modelFor('main.case.order.edit');
     controller.set('order', order);
   }
 });
