c03033400a56ec5feea963f0324c899583670c7d
Erika Pauwels <erika.pauwels@gmail.com>
2018-10-25 10:48:34 +0200

    Fix typo

--- app/config/snippets.js
+++ app/config/snippets.js
@@ -5,6 +5,6 @@ export default {
   },
   'FRA': {
     offerDocumentIntro: `<p class="aanhef">Monsieur, Madame,</p><div>Suite à votre demande, nous avons l'avantage de vous faire offre pour fourniture et placement de:</div>`,
-    offerDocumentOutro: '<div>Option : 1 mois, après sans engagement</div><div>Payement : 30% à la commande, solde après placement.</div>'
+    offerDocumentOutro: '<div>Option : 1 mois, après sans engagement</div><div>Paiement : 30% à la commande, solde après placement.</div>'
   }
 };
