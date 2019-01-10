42a0792e6c8d69693f37ee1f94e1565c3542fa75
Erika Pauwels <erika.pauwels@gmail.com>
2019-01-10 17:27:49 +0100

    Don't specify unnecessary inverses in ember data models

--- app/models/country.js
+++ app/models/country.js
@@ -4,7 +4,6 @@ export default DS.Model.extend({
   code: DS.attr(),
   name: DS.attr(),
   telephonePrefix: DS.attr(),
-  telephones: DS.hasMany('telephone'),
   contacts: DS.hasMany('contact'),
   buildings: DS.hasMany('building'),
   customers: DS.hasMany('customer')
--- app/models/telephone-type.js
+++ app/models/telephone-type.js
@@ -1,6 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name: DS.attr(),
-  telephones: DS.hasMany('telephone')
+  name: DS.attr()
 });
