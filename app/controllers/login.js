28a881e37c7617aea4c8571edccd85685de2a998
Erika Pauwels <erika.pauwels@gmail.com>
2019-08-21 14:39:55 +0200

    Add popup help message during login

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,14 +1,17 @@
-import { inject } from '@ember/service';
 import Controller from '@ember/controller';
+import { inject } from '@ember/service';
+import { task } from 'ember-concurrency';
 
 export default Controller.extend({
   session: inject(),
+
+  login: task(function * () {
+    yield this.session.authenticate('authenticator:torii', 'azure-ad2-oauth2').catch((reason) => {
+      this.set('errorMessage', reason.error || reason);
+    });
+  }).restartable(),
+
   actions: {
-    login() {
-      this.session.authenticate('authenticator:torii', 'azure-ad2-oauth2').catch((reason) => {
-        this.set('errorMessage', reason.error || reason);
-      });
-    },
     logout() {
       this.session.invalidate();
     },
