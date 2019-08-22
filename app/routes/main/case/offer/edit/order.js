90beeee3b8779623c348d306989ac85796b0315c
Erika Pauwels <erika.pauwels@gmail.com>
2019-08-22 11:51:40 +0200

    Redirect to order if it already exists on creation of a new order

--- app/routes/main/case/offer/edit/order.js
+++ app/routes/main/case/offer/edit/order.js
@@ -1,7 +1,17 @@
 import Route from '@ember/routing/route';
+import { debug } from '@ember/debug';
 
 export default Route.extend({
-  async model() {
+  async beforeModel() {
+    const offer = this.modelFor('main.case.offer.edit');
+    const order = await offer.order;
+    if (order) {
+      const customer = this.modelFor('main.case');
+      debug(`Order already exists for offer ${offer.id}. Transition directly to order edit route.`);
+      this.transitionTo('main.case.order.edit', customer, order);
+    }
+  },
+  model() {
     return this.modelFor('main.case.offer.edit');
   }
 });
