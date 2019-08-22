ffb3742f2d5d19a9bfbd318cb66921d913e2d028
Erika Pauwels <erika.pauwels@gmail.com>
2019-08-22 17:08:02 +0200

    Sent frontend errors to the backend

--- app/services/app-state.js
+++ app/services/app-state.js
@@ -1,5 +1,7 @@
 import Service from '@ember/service';
 import { inject as service } from '@ember/service';
+import fetch from 'fetch';
+import config from '../config/environment';
 
 export default Service.extend({
   router: service(),
@@ -10,6 +12,23 @@ export default Service.extend({
   reportError(error) {
     this.set('lastError', error);
     this.set('lastSuccessUrl', this.router.currentURL);
-    // TODO send error to backend
+
+    fetch('/api/error-notifications', {
+      method: 'POST',
+      body: JSON.stringify({
+        data: {
+          attributes: {
+            version: config.APP.version,
+            file: error.fileName,
+            lineNumber: error.lineNumber,
+            columnNumber: error.columnNumber,
+            currentUrl: this.lastSuccessUrl,
+            type: error.name,
+            message: error.message,
+            stack: error.stack
+          }
+        }
+      })
+    });
   }
 });
