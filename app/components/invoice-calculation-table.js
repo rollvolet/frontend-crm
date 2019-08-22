d337195b6dc74bf9d3eef765c553b6b328367e7c
Erika Pauwels <erika.pauwels@gmail.com>
2019-08-22 21:11:35 +0200

    Reformat invoice calculation table with intermediate calculations

--- app/components/invoice-calculation-table.js
+++ app/components/invoice-calculation-table.js
@@ -1,4 +1,5 @@
 import Component from '@ember/component';
+import { oneWay } from '@ember/object/computed';
 import { mapBy } from 'ember-awesome-macros/array';
 import {
   add,
@@ -11,18 +12,20 @@ import raw from 'ember-macro-helpers/raw';
 
 export default Component.extend({
   showSupplementsDialog: false,
-  supplementsAmount: sum(mapBy('model.supplements', raw('amount'))),
-  depositInvoicesAmount: sum(mapBy('model.depositInvoices', raw('arithmeticAmount'))),
-  subtotalAmount: subtract(add('model.baseAmount', 'supplementsAmount'), 'depositInvoicesAmount'),
-  depositsAmount: sum(mapBy('model.deposits', raw('amount'))),
-  totalNetAmount: subtract('subtotalAmount', 'depositsAmount'),
   vatRate: quotient('model.vatRate.rate', 100),
-  baseAmountVat: product('model.baseAmount', 'vatRate'),
+  baseAmount: oneWay('model.baseAmount'),
+  baseAmountVat: product('baseAmount', 'vatRate'),
+  supplementsAmount: sum(mapBy('model.supplements', raw('amount'))),
   supplementsVat: product('supplementsAmount', 'vatRate'),
+  totalOrderAmount: add('model.baseAmount', 'supplementsAmount'),
+  totalOrderVat: product('totalOrderAmount', 'vatRate'),
+  depositInvoicesAmount: sum(mapBy('model.depositInvoices', raw('arithmeticAmount'))),
   // assumption that all deposit invoices have the same vat rate as the parent invoice
   depositInvoicesVat: product('depositInvoicesAmount', 'vatRate'),
-  subtotalVat: product('subtotalAmount', 'vatRate'),
-  grossTotalAmount: subtract(sum('subtotalAmount', 'subtotalVat'), 'depositsAmount'),
+  depositsAmount: sum(mapBy('model.deposits', raw('amount'))),
+  totalNetAmount: subtract('totalOrderAmount', 'depositInvoicesAmount', 'depositsAmount'),
+  totalVat: subtract('totalOrderVat', 'depositInvoicesVat'),
+  grossTotalAmount: sum('totalNetAmount', 'totalVat'),
   actions: {
     openSupplementsDialog() {
       this.set('showSupplementsDialog', true);
