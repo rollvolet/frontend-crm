f2f65e26c3c0b81f6406640e9974e572c775c1dd
Erika Pauwels <erika.pauwels@gmail.com>
2019-08-27 09:44:07 +0200

    Add authentication to request to log frontend error

--- app/services/app-state.js
+++ app/services/app-state.js
@@ -5,6 +5,7 @@ import config from '../config/environment';
 
 export default Service.extend({
   router: service(),
+  session: service(),
 
   lastError: null,
   lastSuccessUrl: null,
@@ -13,16 +14,23 @@ export default Service.extend({
     this.set('lastError', error);
     this.set('lastSuccessUrl', this.router.currentURL);
 
+    const { access_token } = this.session.data.authenticated;
     fetch('/api/error-notifications', {
       method: 'POST',
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+        'Content-Type': 'application/json'
+      },
       body: JSON.stringify({
         data: {
+          type: 'error-notifications',
           attributes: {
             version: config.APP.version,
             file: error.fileName,
-            lineNumber: error.lineNumber,
-            columnNumber: error.columnNumber,
-            currentUrl: this.lastSuccessUrl,
+            'line-number': error.lineNumber,
+            'column-number': error.columnNumber,
+            'current-url': window.location.toString(),
+            'current-path': this.lastSuccessUrl,
             type: error.name,
             message: error.message,
             stack: error.stack
