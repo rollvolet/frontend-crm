323a1cde8eb3e7f92d439a466108485c1426950a
Erika Pauwels <erika.pauwels@gmail.com>
2019-08-27 09:43:52 +0200

    Redirect to index page if there is no error

--- app/routes/oops.js
+++ app/routes/oops.js
@@ -4,6 +4,11 @@ import { inject as service } from '@ember/service';
 export default Route.extend({
   appState: service(),
 
+  beforeModel() {
+    if (!this.appState.lastError)
+      this.transitionTo('main.index');
+  },
+
   model() {
     return this.appState.lastError;
   }
