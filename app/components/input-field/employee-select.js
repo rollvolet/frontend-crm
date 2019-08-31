3693c9b3e8673f1e09fa2223be7b6e1260ba87ec
Erika Pauwels <erika.pauwels@gmail.com>
2019-08-31 15:12:45 +0200

    Refactor filters on employee select component

--- app/components/input-field/employee-select.js
+++ app/components/input-field/employee-select.js
@@ -2,48 +2,50 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { sort } from 'ember-awesome-macros/array';
+import { or } from 'ember-awesome-macros';
 import { proxyAware } from '../../utils/proxy-aware';
 
-const groupByFunction = function(arr) { // eslint-disable-line no-unused-vars
-  return arr.reduce(function(grouped, e) {
-    const group = e.function ? e.function[0] : '';
-    (grouped[group] = grouped[group] || []).push(e);
-    return grouped;
-  }, {});
-};
+const filterKeys = ['isTechnician', 'isAdministrative', 'isOnRoad'];
 
 export default Component.extend({
   store: service(),
 
   selected: proxyAware('value'),
-  employees: computed('onlyActive', 'onlyWithFunction', 'type', 'function', function() {
+  employees: computed('isActive', 'isTechnician', 'isAdministrative', 'isOnRoad', function() {
     let employees = this.store.peekAll('employee');
 
-    if (this.onlyActive)
+    if (this.isActive)
       employees = employees.filter(e => e.active);
 
-    if (this.onlyWithFunction)
-      employees = employees.filter(e => e.function);
-
-    if (this.type != null)
-      employees = employees.filter(e => e.type == this.type);
-
-    if (this.functions != null)
-      employees = employees.filter(e => e.function && this.functions.includes(e.function.substr(0, 1)));
+    const enabledFilters = filterKeys.filter(key => this.get(key));
+    console.trace(enabledFilters);
+    if (enabledFilters.length) {
+      const matches = function(employee) {
+        for (let key of enabledFilters) {
+          if (employee.get(key))
+            return true;
+        }
+        return false;
+      };
+      employees = employees.filter(e => matches(e));
+    }
 
     return employees;
   }),
-  options: sort('employees', ['function:asc']),
+  options: sort('employees', ['functionSort:asc']),
   placeholder: computed('label', 'required', function() {
     return this.required ? `${this.label} *` : this.label;
   }),
   label: 'Werknemer',
   type: null,
   function: null,
-  onlyActive: true,
-  onlyWithFunction: true,
   value: null,
   errors: null,
   required: false,
-  onSelectionChange: null
+  onSelectionChange: null,
+
+  isActive: true,
+  isTechnician: false,
+  isAdministrative: false,
+  isOnRoad: false
 });
--- app/models/employee.js
+++ app/models/employee.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
+import { and, conditional, eq, or, raw } from 'ember-awesome-macros';
 
 export default DS.Model.extend({
   type: DS.attr(),
@@ -8,5 +9,10 @@ export default DS.Model.extend({
   comment: DS.attr(),
   active: DS.attr(),
   function: DS.attr(),
-  workingHours: DS.hasMany('working-hour')
+  workingHours: DS.hasMany('working-hour'),
+
+  functionSort: conditional('function', 'function', raw('Z')),
+  isTechnician: eq('type', raw(2)),
+  isAdministrative: or(eq('type', raw(0)), eq('type', raw(1))),
+  isOnRoad: and(eq('type', raw(1)), eq('function', raw('B')))
 });
