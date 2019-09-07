396c8809b1fbcb432050aaee756399d6ac332ede
Erika Pauwels <erika.pauwels@gmail.com>
2019-09-07 22:00:52 +0200

    Be more defensive in the highlight-substr helper

--- app/helpers/highlight-substr.js
+++ app/helpers/highlight-substr.js
@@ -2,7 +2,11 @@ import { htmlSafe } from '@ember/string';
 import { helper } from '@ember/component/helper';
 
 export function highlightSubstr([text, termToHighlight]/*, hash*/) {
-  return htmlSafe(text.replace(new RegExp(termToHighlight, 'i'), '<b>$&</b>'));
+  if (text) {
+    return htmlSafe(text.replace(new RegExp(termToHighlight, 'i'), '<b>$&</b>'));
+  } else {
+    return text;
+  }
 }
 
 export default helper(highlightSubstr);
