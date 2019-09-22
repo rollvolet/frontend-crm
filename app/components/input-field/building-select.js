c979e0b421a068b17238c31568605fbb0389335b
Erika Pauwels <erika.pauwels@gmail.com>
2019-09-22 22:01:21 +0200

    Workaround ember-data thinking it already loaded a relationship

--- app/components/input-field/building-select.js
+++ app/components/input-field/building-select.js
@@ -8,8 +8,14 @@ export default Component.extend({
   selected: proxyAware('value'),
 
   didReceiveAttrs() {
-    if (this.customer)
-      this.get('customer.buildings').then((buildings) => this.set('options', buildings));
+    if (this.customer) {
+      // TODO replace ember-data-has-many-query with ember-storefront
+      // By using query we force ember-data to reload the relationship.
+      // Ember data may otherwise assume it has already loaded the relation when it only fetched 1 page
+      this.customer.query('buildings', {
+        page: { size: 1000 }
+      }).then((buildings) => this.set('options', buildings));
+    }
   },
 
   customer: null,
--- app/components/input-field/contact-select.js
+++ app/components/input-field/contact-select.js
@@ -8,8 +8,14 @@ export default Component.extend({
   selected: proxyAware('value'),
 
   didReceiveAttrs() {
-    if (this.customer)
-      this.get('customer.contacts').then((contacts) => this.set('options', contacts));
+    if (this.customer) {
+      // TODO replace ember-data-has-many-query with ember-storefront
+      // By using query we force ember-data to reload the relationship.
+      // Ember data may otherwise assume it has already loaded the relation when it only fetched 1 page
+      this.customer.query('contacts', {
+        page: { size: 1000 }
+      }).then((contacts) => this.set('options', contacts));
+    }
   },
 
   customer: null,
