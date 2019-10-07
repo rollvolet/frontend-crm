efa2e84fd49457b3c04779e2b89bdc719c6fad5d
Erika Pauwels <erika.pauwels@gmail.com>
2019-10-07 07:22:53 +0200

    Fix typo in offer snippet

--- app/config/snippets.js
+++ app/config/snippets.js
@@ -5,6 +5,6 @@ export default {
   },
   'FRA': {
     offerDocumentIntro: `<p class="aanhef">Madame, Monsieur,</p><div>Suite à votre demande, nous avons l'avantage de vous faire notre meilleure offre pour fourniture et placement de:</div>`,
-    offerDocumentOutro: `<p>Cette offre est valable pendant 30 jours.</p><div>Si vous êtes d'accord, veuillez nous envoyer votre confirmation par écrite et verser un <b>acompte de 30%</b> sur la commande complète sur notre compte <b>BE24 4347 1829 8138</b> en mentionnant la référence de cette offre.</div>`
+    offerDocumentOutro: `<p>Cette offre est valable pendant 30 jours.</p><div>Si vous êtes d'accord, veuillez nous envoyer votre confirmation par écrit et verser un <b>acompte de 30%</b> sur la commande complète sur notre compte <b>BE24 4347 1829 8138</b> en mentionnant la référence de cette offre.</div>`
   }
 };
