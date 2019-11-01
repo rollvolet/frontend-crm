b4cf1ea65ff58a295dfeb90aef133dd194410a87
Erika Pauwels <erika.pauwels@gmail.com>
2019-11-01 10:11:38 +0100

    Disable edit of contact/building on entrance of a case route

--- app/routes/main/case.js
+++ app/routes/main/case.js
@@ -12,5 +12,7 @@ export default Route.extend({
   setupController(controller) {
     this._super(...arguments);
     controller.set('memoExpanded', false);
+    controller.set('isEnabledEditBuilding', false);
+    controller.set('isEnabledEditContact', false);
   }
 });
