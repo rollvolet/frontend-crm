2118bab326f575ced46e8ff63ccc0642bf6029a3
Erika Pauwels <erika.pauwels@gmail.com>
2019-11-11 17:26:13 +0100

    Set current logged in employee as default employee on a new request

---
+++ app/services/current-session.js
@@ -0,0 +1,25 @@
+import Service, { inject } from '@ember/service';
+
+export default Service.extend({
+  session: inject(),
+  store: inject(),
+
+  employee: undefined,
+
+  async getCurrentEmployee() {
+    if (this.employee === undefined) {
+      const username = this.session.get('data.authenticated.user.name');
+      if (username) {
+        const firstName = username.split(' ')[0].toLowerCase();
+        const employees = await this.store.findAll('employee');
+        const employee = employees.find(e => e.firstName.toLowerCase() == firstName);
+        this.set('employee', employee);
+      } else {
+        this.set('employee', null);
+      }
+      console.log('Initialized employee');
+      console.trace(this.employee);
+    }
+    return this.employee;
+  }
+});
