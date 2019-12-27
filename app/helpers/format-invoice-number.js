bd59055c8de0a5b360664028ccbf3d40399e423e
Erika Pauwels <erika.pauwels@gmail.com>
2019-12-27 18:54:05 +0100

    Add support for invoice numbers with more than 7 digits

--- app/helpers/format-invoice-number.js
+++ app/helpers/format-invoice-number.js
@@ -2,10 +2,12 @@ import { helper } from '@ember/component/helper';
 
 export function formatInvoiceNumber(value) {
   const valueStr = (value && value.toString()) || '';
-  if (valueStr.length > 2)
-    return `${valueStr.substring(0,2)}/${valueStr.substring(2)}`;
-  else
+  if (valueStr.length > 4) {
+    const i = valueStr.length - 4;
+    return `${valueStr.substring(0, i)}/${valueStr.substring(i)}`;
+  } else {
     return valueStr;
+  }
 }
 
 export function deformatInvoiceNumber(formattedNumber) {
