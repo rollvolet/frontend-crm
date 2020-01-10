2385c98bf6a8f7d778a3db555dd4ae7235c7312d
Erika Pauwels <erika.pauwels@gmail.com>
2020-01-10 17:18:50 +0100

    Refactor to Glimmer components

--- app/components/save-status-icon.js
+++ app/components/save-status-icon.js
@@ -1,11 +1,5 @@
-import classic from 'ember-classic-decorator';
-import { classNames, tagName } from '@ember-decorators/component';
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 
-@classic
-@tagName('span')
-@classNames('save-status')
 export default class SaveStatusIcon extends Component {
-  task = null;
-  model = null;
+
 }
