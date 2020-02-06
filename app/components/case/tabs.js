6fa158399b0d7af311d0ddb125e21a062e584f2a
Erika Pauwels <erika.pauwels@gmail.com>
2020-02-06 12:55:46 +0100

    Refactor case/tabs to a Glimmer component

--- app/components/case/tabs.js
+++ app/components/case/tabs.js
@@ -1,25 +1,18 @@
-import classic from 'ember-classic-decorator';
-import { action, computed } from '@ember/object';
+import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
-import { alias } from '@ember/object/computed';
-import Component from '@ember/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
-@classic
-export default class CaseTabs extends Component {
-  @service
-  case;
+export default class CaseTabsComponent extends Component {
+  @service case
+  @service router
+  @service store
 
-  @service
-  router;
+  @tracked currentRouteName
+  @tracked isEditRoute
 
-  @service
-  store;
-
-  currentRouteName = null;
-  isEditRoute = null;
-
-  init() {
-    super.init(...arguments);
+  constructor() {
+    super(...arguments);
     this.case.initCase();
 
     // computed properties on this.router.currentRoute don't seem to work
@@ -28,39 +21,38 @@ export default class CaseTabs extends Component {
     this.currentRouteChanged(this.router, 'currentRoute'); // initialize derived values
   }
 
-  willDestroyElement() {
-    super.willDestroyElement(...arguments);
+  willDestroy() {
+    super.willDestroy(...arguments);
     this.router.removeObserver('currentRoute', this, 'currentRouteChanged');
   }
 
+  // observation handler
   currentRouteChanged(sender, key) {
     const currentRoute = sender[key];
-    this.set('currentRouteName', currentRoute.name);
-    this.set('isEditRoute', currentRoute.queryParams.editMode == "true");
+    this.currentRouteName = currentRoute.name;
+    this.isEditRoute = currentRoute.queryParams.editMode == "true";
   }
 
-  @alias('case.current')
-  model;
+  get model() {
+    return this.case.current;
+  }
 
-  @alias('model.request.visitor')
-  visitorName;
+  get visitorName() {
+    return this.model.request && this.model.request.visitor;
+  }
 
-  @computed('visitorName')
   get visitor() {
     return this.store.peekAll('employee').find(e => e.firstName == this.visitorName);
   }
 
-  @computed('isEditRoute', 'model', 'model.{requestId,offerId}')
   get canCreateNewOffer() {
     return !this.isEditRoute && this.model && this.model.requestId && this.model.offerId == null;
   }
 
-  @computed('isEditRoute', 'model', 'model.{offerId,orderId,offer}')
   get canCreateNewOrder() {
     return !this.isEditRoute && this.model && this.model.offerId && this.model.orderId == null && this.model.offer && !this.model.offer.isMasteredByAccess;
   }
 
-  @computed('isEditRoute', 'model', 'model.{orderId,invoiceId,order}')
   get canCreateNewInvoice() {
     return !this.isEditRoute && this.model && this.model.orderId && this.model.invoiceId == null && this.model.order && !this.model.order.isMasteredByAccess;
   }
