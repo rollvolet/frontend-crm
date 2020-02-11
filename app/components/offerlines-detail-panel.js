28590c59bb0d5e2734ccd91a67258c27f02715d3
Erika Pauwels <erika.pauwels@gmail.com>
2020-02-11 08:12:31 +0100

    Remove deprecated offer components

--- app/components/offerlines-detail-panel.js
+++
@@ -1,7 +0,0 @@
-import Component from '@glimmer/component';
-
-export default class OfferlinesDetailPanel extends Component {
-  get sortedLines() {
-    return this.args.model.sortBy('sequenceNumber');
-  }
-}
