1adcdf6f828bc07896b8fc547e4a0bc6be24be04
Erika Pauwels <erika.pauwels@gmail.com>
2020-02-28 08:28:12 +0100

    Fix invoicelines for orders without an invoice

--- app/components/invoiceline/table.js
+++ app/components/invoiceline/table.js
@@ -17,7 +17,7 @@ export default class InvoicelineTableComponent extends Component {
   }
 
   get invoice() {
-    return this.invoicelines.firstObject.invoice;
+    return this.invoicelines.firstObject && this.invoicelines.firstObject.invoice;
   }
 
   @keepLatestTask
@@ -25,8 +25,7 @@ export default class InvoicelineTableComponent extends Component {
     this.invoicelines = yield this.args.model;
     // Load data that is already loaded by the invoice/panel component
     yield all(this.invoicelines.map(line => line.sideload('order,invoice,vat-rate', { backgroundReload: false })));
-    const invoice = yield this.invoicelines.firstObject.invoice;
-    const order = invoice && (yield invoice.order);
+    const order = this.invoice && (yield this.invoice.order);
     this.enrichedInvoicelines = yield all(this.invoicelines.map(async (line) => {
       line.isSupplement = order && !(await line.get('order'));
       return line;
