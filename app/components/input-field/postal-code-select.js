6f19290e1b6275f23b0a75c6456cef752e8bf1c6
Erika Pauwels <erika.pauwels@gmail.com>
2020-04-04 16:30:17 +0200

    Refactor postal-code-select to Glimmer component

--- app/components/input-field/postal-code-select.js
+++ app/components/input-field/postal-code-select.js
@@ -1,63 +1,81 @@
-import classic from 'ember-classic-decorator';
+import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
-import Component from '@ember/component';
-import { task, timeout } from 'ember-concurrency';
-import { or, isEmpty } from 'ember-awesome-macros';
+import { isEmpty } from '@ember/utils';
+import { timeout } from 'ember-concurrency';
+import { keepLatestTask } from 'ember-concurrency-decorators';
 
-@classic
 export default class PostalCodeSelect extends Component {
-  @service
-  store;
+  @service store
 
-  @or(isEmpty('newCode'), isEmpty('newCity'))
-  isAddOptionDisabled;
+  @tracked value
+  @tracked postalCodes = []
+  @tracked showCreateModal = false;
+  @tracked newCode
+  @tracked newCity
 
-  init() {
-    super.init(...arguments);
-    const postalCodes = this.store.peekAll('postal-code');
-    this.set('postalCodes', postalCodes);
-    this.set('options', this.postalCodes.slice(0, this.size));
-  }
+  constructor() {
+    super(...arguments);
 
-  didInsertElement() {
-    super.didInsertElement(...arguments);
-    if (this.postalCode && this.city) {
-      const value = this.postalCodes.find(o => o.code == this.postalCode && o.name == this.city.toUpperCase());
+    this.postalCodes = this.store.peekAll('postal-code');
+
+    if (this.args.postalCode && this.args.city) {
+      const value = this.postalCodes.find(o => o.code == this.args.postalCode && o.name == this.args.city.toUpperCase());
 
       if (value) {
-        this.set('value', value);
+        this.value = value;
       } else {
         const postalCode = this.store.createRecord('postal-code', {
-          code: this.postalCode,
-          name: this.city.toUpperCase()
+          code: this.args.postalCode,
+          name: this.args.city.toUpperCase()
         });
-        this.set('value', postalCode);
+        this.value = postalCode;
       }
     }
   }
 
-  @(task(function* (term) {
+  get label() {
+    return this.args.label || 'Gemeente';
+  }
+
+  get required() {
+    return this.args.required || false;
+  }
+
+  get placeholder() {
+    return this.required ? `${this.label} *` : this.label;
+  }
+
+  get size() {
+    return this.args.size || 50;
+  }
+
+  get isAddOptionDisabled() {
+    return isEmpty('newCode') || isEmpty('newCity');
+  }
+
+  get titleize() {
+    return this.args.titleize || false;
+  }
+
+  get options() {
+    return this.postalCodes.slice(0, this.size);
+  }
+
+  @keepLatestTask
+  *search(term) {
     yield timeout(100);
     return this.postalCodes.filter(p => p.search.toLowerCase().includes(term)).slice(0, this.size);
-  }).keepLatest())
-  search;
-
-  label = 'Gemeente';
-  value = null;
-  postalCode = null;
-  city = null;
-  onSelectionChange = null;
-  size = 50;
-  titleize = false;
+  }
 
   @action
   selectValue(value) {
-    this.set('value', value);
+    this.value = value;
     if (value)
-      this.onSelectionChange(value.code, value.name);
+      this.args.onSelectionChange(value.code, value.name);
     else
-      this.onSelectionChange(undefined, undefined);
+      this.args.onSelectionChange(undefined, undefined);
   }
 
   @action
@@ -66,10 +84,10 @@ export default class PostalCodeSelect extends Component {
       code: this.newCode,
       name: this.newCity.toUpperCase()
     });
-    this.set('value', postalCode);
-    this.onSelectionChange(postalCode.code, postalCode.name);
-    this.set('showCreateModal', false);
-    this.set('newCode', null);
-    this.set('newCity', null);
+    this.value = postalCode;
+    this.args.onSelectionChange(postalCode.code, postalCode.name);
+    this.showCreateModal = false;
+    this.newCode = null;
+    this.newCity = null;
   }
 }
--- app/components/input-field/postal-code-select/create-new.js
+++ app/components/input-field/postal-code-select/create-new.js
@@ -1,14 +1,10 @@
-import classic from 'ember-classic-decorator';
+import Component from '@glimmer/component';
 import { action } from '@ember/object';
-import { tagName } from '@ember-decorators/component';
-import Component from '@ember/component';
 
-@classic
-@tagName('')
 export default class CreateNew extends Component {
   @action
   create() {
-    this.onCreate();
-    this.select.actions.close();
+    this.args.onCreate();
+    this.args.select.actions.close();
   }
 }
