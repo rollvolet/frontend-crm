b40587383dad0ed1a4dcb15b4d08d8ced4e69bae
Erika Pauwels <erika.pauwels@gmail.com>
2020-04-04 12:06:55 +0200

    Refactor input-field/language-select to glimmer component

--- app/components/input-field/language-select.js
+++ app/components/input-field/language-select.js
@@ -1,29 +1,25 @@
-import classic from 'ember-classic-decorator';
-import { computed } from '@ember/object';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
-import Component from '@ember/component';
-import { proxyAware } from '../../utils/proxy-aware';
 
-@classic
 export default class LanguageSelect extends Component {
   @service store
 
-  @proxyAware('value')
-  selected
+  @tracked options = []
 
-  init() {
-    super.init(...arguments);
-    const languages = this.store.peekAll('language');
-    this.set('options', languages);
+  constructor() {
+    super(...arguments);
+    this.options = this.store.peekAll('language');
   }
 
-  label = 'Taal';
-  value = null;
-  errors = null;
-  required = false;
-  onSelectionChange = null;
+  get label() {
+    return this.args.label || 'Taal';
+  }
 
-  @computed('label', 'required')
+  get required() {
+    return this.args.required || false;
+  }
+
   get placeholder() {
     return this.required ? `${this.label} *` : this.label;
   }
