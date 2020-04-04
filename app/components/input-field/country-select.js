b3026d1410e5ed7330d62dad459682fd61a88235
Erika Pauwels <erika.pauwels@gmail.com>
2020-04-04 12:11:29 +0200

    Refactor input-fields/country-select to glimmer component

--- app/components/input-field/country-select.js
+++ app/components/input-field/country-select.js
@@ -1,29 +1,25 @@
-import classic from 'ember-classic-decorator';
-import { computed } from '@ember/object';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
-import Component from '@ember/component';
-import { proxyAware } from '../../utils/proxy-aware';
 
-@classic
-export default class CountrySelect extends Component {
-  @service store;
+export default class LanguageSelect extends Component {
+  @service store
 
-  @proxyAware('value')
-  selected;
+  @tracked options = []
 
-  init() {
-    super.init(...arguments);
-    const countries = this.store.peekAll('country');
-    this.set('options', countries);
+  constructor() {
+    super(...arguments);
+    this.options = this.store.peekAll('country');
   }
 
-  label = 'Land';
-  value = null;
-  errors = null;
-  required = false;
-  onSelectionChange = null;
+  get label() {
+    return this.args.label || 'Land';
+  }
 
-  @computed('label', 'required')
+  get required() {
+    return this.args.required || false;
+  }
+
   get placeholder() {
     return this.required ? `${this.label} *` : this.label;
   }
