23b86fb086c076cda290ddbb90fecd4a191772b5
Erika Pauwels <erika.pauwels@gmail.com>
2020-04-04 16:32:29 +0200

    Fix edit action in customer/buildings-table

--- app/components/customer/buildings-table.js
+++ app/components/customer/buildings-table.js
@@ -1,6 +1,5 @@
 import FilterComponent from '../data-table-filter';
 import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { restartableTask } from 'ember-concurrency-decorators';
 
@@ -61,10 +60,5 @@ export default class BuildingsTable extends FilterComponent {
         telephone: filter.telephone
       }
     });
-  }
-
-  @action
-  edit(building) {
-    this.onEdit(building);
   }
 }
