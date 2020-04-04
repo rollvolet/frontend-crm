6c9e093d8d9cfacaca0a06cbed200297f5427ee6
Erika Pauwels <erika.pauwels@gmail.com>
2020-04-04 15:49:20 +0200

    Refactor input-fields/payment-select to Glimmer component

--- app/components/input-field/payment-select.js
+++ app/components/input-field/payment-select.js
@@ -1,23 +1,26 @@
-import classic from 'ember-classic-decorator';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
-import Component from '@ember/component';
-import { proxyAware } from '../../utils/proxy-aware';
 
-@classic
 export default class PaymentSelect extends Component {
-  @service
-  store;
+  @service store
 
-  @proxyAware('value')
-  selected;
+  @tracked options = []
 
-  init() {
-    super.init(...arguments);
-    const payments = this.store.peekAll('payment');
-    this.set('options', payments);
+  constructor() {
+    super(...arguments);
+    this.options = this.store.peekAll('payment');
   }
 
-  label = 'Bank';
-  value = null;
-  onSelectionChange = null;
+  get label() {
+    return this.args.label || 'Bank';
+  }
+
+  get required() {
+    return this.args.required || false;
+  }
+
+  get placeholder() {
+    return this.required ? `${this.label} *` : this.label;
+  }
 }
