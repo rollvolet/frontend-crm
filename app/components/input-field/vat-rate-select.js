d0ca49b90866aaf3ee4455428f6afabb8705675a
Erika Pauwels <erika.pauwels@gmail.com>
2020-04-04 13:24:51 +0200

    Refactor input-fields/vat-rate-select to Glimmer component

--- app/components/input-field/vat-rate-select.js
+++ app/components/input-field/vat-rate-select.js
@@ -1,34 +1,29 @@
-import classic from 'ember-classic-decorator';
-import { computed } from '@ember/object';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
-import Component from '@ember/component';
-import { proxyAware } from '../../utils/proxy-aware';
 
-@classic
 export default class VatRateSelect extends Component {
-  @service
-  store;
+  @service store
 
-  @proxyAware('value')
-  selected;
+  @tracked options = []
 
-  init() {
-    super.init(...arguments);
-    const vatRates = this.store.peekAll('vat-rate');
-    this.set('options', vatRates);
+  constructor() {
+    super(...arguments);
+    this.options = this.store.peekAll('vat-rate');
   }
 
-  label = 'BTW tarief';
-  value = null;
-  errors = null;
-  required = false;
-  onSelectionChange = null;
-
-  get allowClearWithDefault() {
-    return this.allowClear != null ? this.allowClear : true;
+  get label() {
+    return this.args.label || 'BTW tarief';
   }
 
-  @computed('label', 'required')
+  get required() {
+    return this.args.required || false;
+  }
+
+  get allowClear() {
+    return this.args.allowClear || true;
+  }
+
   get placeholder() {
     return this.required ? `${this.label} *` : this.label;
   }
