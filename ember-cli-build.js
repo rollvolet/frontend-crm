8cdf52b0db84cfbe99da2d38ffb2db51de31e7d9
Erika Pauwels <erika.pauwels@gmail.com>
2020-04-05 12:12:56 +0200

    Improve ember-cli-build configuration for development

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,24 +1,46 @@
 'use strict';
+// Credits to the following posts that helps me to reduce build times drastically
 
+// https://discuss.emberjs.com/t/tips-for-improving-build-time-of-large-apps/15008/12
+// https://www.gokatz.me/blog/how-we-cut-down-our-ember-build-time/
+
+
+//ember-cli-build.js
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+let env = EmberApp.env(),
+  IS_PROD = env === 'production',
+  IS_TEST = env === 'test' ;
+
+const nodeSass = require('node-sass');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    // hinting: IS_TEST, // Disable linting for all builds but test
+    tests: IS_TEST, // Don't even generate test files unless a test build
+    "ember-cli-babel": {
+      includePolyfill: IS_PROD // Only include babel polyfill in prod
+    },
+    autoprefixer: {
+      sourcemap: false // Was never helpful
+    },
+    sourcemaps: {
+      enabled: IS_PROD // CMD ALT F in chrome is *almost* as fast as CMD P
+    },
+    fingerprint: {
+      enabled: IS_PROD //Asset rewrite will takes more time and fingerprinting can be omitted in development
+    },
+    sassOptions: {
+      // moving from compass compiler to node gave huge improvement
+      implementation: nodeSass, //implementation here is node-sass,
+      sourceMap : false //will debug with generated CSS than sourcemap :)
+    }
   });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+  // Only import other polyfills in production
+  if (IS_PROD) {
+    app.import('vendor/ie-polyfill.js');
+  }
 
   return app.toTree();
 };
