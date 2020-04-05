3008c057ded12a3f6b456575d9a458122db5dd23
Erika Pauwels <erika.pauwels@gmail.com>
2020-04-05 12:06:59 +0200

    Show missing certificate dialog for deposit invoices

--- app/components/deposit-invoice/list-item.js
+++ app/components/deposit-invoice/list-item.js
@@ -1,18 +1,26 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 import { task } from 'ember-concurrency-decorators';
 import { warn } from '@ember/debug';
 
 export default class DepositInvoiceListItemComponent extends Component {
   @service documentGeneration
 
+  @tracked showMissingCertificateDialog = false
+
   @task
   *generateInvoiceDocument() {
-    try {
-      yield this.documentGeneration.invoiceDocument(this.args.model);
-    } catch(e) {
-      warn(`Something went wrong while generating the invoice document`, { id: 'document-generation-failure' });
+    if (!this.showMissingCertificateDialog && this.args.model.certificateRequired && !this.args.model.certificateReceived) {
+      this.showMissingCertificateDialog = true;
+    } else {
+      this.showMissingCertificateDialog = false;
+      try {
+        yield this.documentGeneration.invoiceDocument(this.args.model);
+      } catch (e) {
+        warn(`Something went wrong while generating the invoice document`, { id: 'document-generation-failure' });
+      }
     }
   }
 
@@ -20,5 +28,4 @@ export default class DepositInvoiceListItemComponent extends Component {
   downloadInvoiceDocument() {
     this.documentGeneration.downloadInvoiceDocument(this.args.model);
   }
-
 }
