3c176da77bb5fd92870196723cfa21bd57bdf72e
Erika Pauwels <erika.pauwels@gmail.com>
2020-04-12 18:00:50 +0200

    Fix retrieval of deposit-invoices in model hook

--- app/routes/main/case/order/edit/deposit-invoices.js
+++ app/routes/main/case/order/edit/deposit-invoices.js
@@ -4,7 +4,7 @@ import { action } from '@ember/object';
 export default class DepositInvoicesRoute extends Route {
   model() {
     const order = this.modelFor('main.case.order.edit');
-    return this.store.loadRecords('depositInvoice', {
+    return this.store.query('depositInvoice', {
       sort: '-number',
       page: {
         size: 1000 // we don't expect more than 1000 deposit invoices for 1 order
