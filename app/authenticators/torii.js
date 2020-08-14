d6662627ab31e4c9525dd98c84f36601fdaa9f85
Erika Pauwels <erika.pauwels@gmail.com>
2020-08-14 08:58:27 +0200

    Make login cookie based instead of token based

--- app/authenticators/torii.js
+++ app/authenticators/torii.js
@@ -7,6 +7,8 @@ import { isEmpty } from '@ember/utils';
 import config from '../config/environment';
 import decodeAuthToken from '../utils/decode-auth-token';
 
+const toriiProvider = 'azure-ad2-oauth2';
+
 /**
  * Azure AD 2.0 OAuth2 authenticator that supports automatic token refresh.
  *
@@ -15,17 +17,11 @@ import decodeAuthToken from '../utils/decode-auth-token';
 export default class Torii extends ToriiAuthenticator {
   @service torii
 
-  tokenEndpoint = '/api/authentication/token';
-  refreshTokenEndpoint = '/api/authentication/refresh-token';
-  refreshLeeway = 60; // in seconds
-  refreshTokenTimeout = null;
-  toriiProvider = 'azure-ad2-oauth2';
-
   async authenticate() {
     const data = await super.authenticate(...arguments); // get authorization code through Torii
 
     // exchange authorization code for access token/refresh token
-    const response = await fetch(this.tokenEndpoint, {
+    const result = await fetch('/api/sessions', {
       method: 'POST',
       headers: new Headers({
         'Content-Type': 'application/json'
@@ -37,101 +33,45 @@ export default class Torii extends ToriiAuthenticator {
       })
     });
 
-    if (response.ok) {
-      const responseBody = await response.json();
-      return this.handleAuthResponse(responseBody);
+    if (result.ok) {
+      const response = await result.json();
+      return {
+        provider: toriiProvider, // required to make session restore work
+        user: response
+      };
     } else {
-      throw response;
+      throw result;
     }
-
   }
 
   async invalidate() {
     const response = await super.invalidate(...arguments);
-    cancel(this.refreshTokenTimeout);
-    delete this.refreshTokenTimeout;
+    await fetch('/api/sessions/current', {
+      method: 'DELETE',
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
+    });
     return response;
   }
 
   async restore() {
-    const data = await super.restore(...arguments);
-    const refreshToken = data.refresh_token;
-
-    if (!this.refreshTokenTimeout) {
-      // No refresh token task scheduled in the future. Attempt to refresh the token now.
-      // If the server rejects the token the user session will be invalidated
-      return this.refreshAccessToken(refreshToken);
-    } else {
-      this.scheduleAccessTokenRefresh(data);
-      return data;
-    }
-  }
-
-  async refreshAccessToken(refreshToken) {
-    debug(`Attempt to refresh access token at ${new Date()}`);
-
-    const response = await fetch(this.refreshTokenEndpoint, {
-      method: 'POST',
+    await super.restore(...arguments);
+    const result = await fetch('/api/sessions/current', {
+      method: 'GET',
       headers: new Headers({
         'Content-Type': 'application/json'
-      }),
-      body: JSON.stringify({
-        'refresh-token': refreshToken,
-        'redirect-uri': config.torii.providers['azure-ad2-oauth2'].redirectUri,
-        scope: config.torii.providers['azure-ad2-oauth2'].scope
       })
     });
 
-    if (response.ok) {
-      const responseBody = await response.json();
-      const sessionData = this.handleAuthResponse(responseBody);
-      run(async () => {
-        try {
-          this.trigger('sessionDataUpdated', sessionData);
-        } catch (error) {
-          warn(`Failed to update session data after access token refresh: ${error}`, { id: 'refresh-token.data-update-failure' });
-          await this.invalidate();
-          this.trigger('sessionDataInvalidated');
-        }
-      });
-      return sessionData;
+    if (result.ok) {
+      const response = await result.json();
+      return {
+        provider: toriiProvider, // required to make session restore work
+        user: response
+      };
     } else {
-      throw response;
+      throw result;
     }
-  }
-
-  handleAuthResponse(response) {
-    const expiresAt = this.scheduleAccessTokenRefresh(response);
-
-    const accessToken = response.access_token;
-    const jwt = decodeAuthToken(accessToken);
-    return {
-      provider: this.toriiProvider, // required to make session restore work
-      'access_token': accessToken,
-      'refresh_token': response.refresh_token,
-      'expires_at': expiresAt,
-      user: {
-        account: jwt.preferred_username,
-        name: jwt.name,
-        roles: jwt.roles
-      }
-    };
-  }
-
-  scheduleAccessTokenRefresh({ expires_in, expires_at, refresh_token }) {
-    if (expires_in) {
-      expires_at = (Date.now() + expires_in * 1000) / 1000;
-    }
-    const now = Math.floor((new Date()).getTime() / 1000); // current time
-    const wait = (expires_at - now - this.refreshLeeway) * 1000;
-
-    if (!isEmpty(refresh_token) && !isEmpty(expires_at) && wait > 0) {
-      cancel(this.refreshTokenTimeout);
-      delete this.refreshTokenTimeout;
-      debug(`Refresh token task scheduled in ${wait/1000} seconds at ${new Date(Date.now() + wait)}`);
-      this.refreshTokenTimeout = later(this, this.refreshAccessToken, refresh_token, wait);
-    }
-
-    return expires_at;
   }
 }
