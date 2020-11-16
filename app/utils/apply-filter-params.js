da6ce43e664503d29fee7d176b60f8632e861a4d
Erika Pauwels <erika.pauwels@gmail.com>
2020-11-16 20:19:08 +0100

    Fix integer query params in data-table filters

--- app/utils/apply-filter-params.js
+++ app/utils/apply-filter-params.js
@@ -2,7 +2,11 @@ import { isEmpty } from '@ember/utils';
 
 export default function applyFilterParams(filter) {
   for (let key of Object.keys(filter)) {
-    const value = filter[key] && !isEmpty(filter[key]) ? filter[key] : undefined;
+    let value = undefined;
+    if (Number.isInteger(filter[key]))
+      value = filter[key];
+    else
+      value = filter[key] && !isEmpty(filter[key]) ? filter[key] : undefined;
     this.set(key, value);
   }
   this.set('page', 0); // reset page to 0 on new search
