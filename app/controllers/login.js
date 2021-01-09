40ae1dd07c20260c9ccafb60ceb6633040866eed
Erika Pauwels <erika.pauwels@gmail.com>
2021-01-09 11:45:38 +0100

    Remove unused controller action and properties

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,28 +1,6 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
-import { restartableTask } from 'ember-concurrency-decorators';
 
 export default class LoginController extends Controller {
   @service session
-
-  @tracked errorMessage
-
-  @restartableTask
-  *login() {
-    yield this.session.authenticate('authenticator:torii', 'azure-ad2-oauth2').catch((reason) => {
-      this.errorMessage = reason.error || reason;
-    });
-  }
-
-  @action
-  logout() {
-    this.session.invalidate();
-  }
-
-  @action
-  resetError() {
-    this.errorMessage = null;
-  }
 }
