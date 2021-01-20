ba9f29b24621e0f3f10c09086af8541688916de3
Erika Pauwels <erika.pauwels@gmail.com>
2021-01-20 13:48:25 +0100

    Add comment and technicians to outstanding jobs report

--- app/classes/outstanding-job.js
+++ app/classes/outstanding-job.js
@@ -1,11 +1,21 @@
 import snakeToCamelString from '../utils/snake-to-camel-string';
+import { tracked } from '@glimmer/tracking';
 
 export default class OutstandingJob {
+  @tracked expandComment
+
   constructor(obj) {
     const keys = Object.keys(obj);
     for (let key of keys) {
       const k = snakeToCamelString(key);
       this[k] = obj[key];
     }
+  }
+
+  get techniciansList() {
+    if (this.technicians)
+      return this.technicians.split(',').map(t => t.trim());
+    else
+      return [];
   }
 }
