fea8416aaf4026ce28acfe75a0baab057be617f7
Erika Pauwels <erika.pauwels@gmail.com>
2021-01-22 14:20:53 +0100

    Convert main navigation menu to TailwindCSS

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -1,16 +1,14 @@
+import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-import Controller from '@ember/controller';
 
 export default class MainController extends Controller {
   @service configuration;
   @service session;
   @service userInfo;
 
-  @action
-  openMail() {
-    window.location.href = 'mailto:erika.pauwels@redpencil.io';
-  }
+  @tracked isOpenMenu = false;
 
   @action
   logout() {
@@ -18,7 +16,12 @@ export default class MainController extends Controller {
   }
 
   @action
-  goToProfile() {
-    window.alert("User profile not implemented yet");
+  openMenu() {
+    this.isOpenMenu = true;
+  }
+
+  @action
+  closeMenu() {
+    this.isOpenMenu = false;
   }
 }
