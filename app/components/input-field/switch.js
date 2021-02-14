b36911be367b1e791f058b40c83121147f8c2caa
Erika Pauwels <erika.pauwels@gmail.com>
2021-02-14 17:09:55 +0100

    Add input-field/switch component

---
+++ app/components/input-field/switch.js
@@ -0,0 +1,26 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+
+export default class InputFieldSwitchComponent extends Component {
+  get onValue() {
+    return true;
+  }
+
+  get offValue() {
+    return false;
+  }
+
+  get isOn() {
+    return this.args.value == this.onValue;
+  }
+
+  get isOff() {
+    return !this.isOn;
+  }
+
+  @action
+  toggleValue() {
+    const newValue = this.isOn ? this.offValue : this.onValue;
+    this.args.onChange(newValue);
+  }
+}
