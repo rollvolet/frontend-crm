e861aba167a02c37c1f6e9159d442d6f87500939
Erika Pauwels <erika.pauwels@gmail.com>
2021-02-15 11:56:55 +0100

    Refactor orders data-table to Tailwind CSS

--- app/controllers/main/orders/index.js
+++ app/controllers/main/orders/index.js
@@ -1,21 +1,32 @@
-import { action } from '@ember/object';
 import Controller from '@ember/controller';
-import DefaultQueryParams from 'ember-data-table/mixins/default-query-params';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 import applyFilterParams from '../../../utils/apply-filter-params';
 
-export default class IndexController extends Controller.extend(DefaultQueryParams) {
-  size = 25
-  sort = '-order-date'
-
-  @action
-  clickRow(row) {
-    const customerId = row.get('customer.id');
-    const orderId = row.get('id');
-    this.transitionToRoute('main.case.order.edit', customerId, orderId);
-  }
+export default class IndexController extends Controller {
+  @tracked page = 0;
+  @tracked size = 25;
+  @tracked sort = '-order-date';
+  @tracked withoutInvoice = false; // enforce boolean datatype
+  @tracked onlyNotCanceled = false; // enforce boolean datatype
 
   @action
   applyFilter(filter) {
     applyFilterParams.bind(this)(filter);
+  }
+
+  @action
+  previousPage() {
+    this.selectPage(this.page - 1);
+  }
+
+  @action
+  nextPage() {
+    this.selectPage(this.page + 1);
+  }
+
+  @action
+  selectPage(page) {
+    this.page = page;
   }
 }
