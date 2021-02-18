7383c5bf930f2eb1e1ef5f69744219d3c64e5865
Erika Pauwels <erika.pauwels@gmail.com>
2021-02-18 15:34:20 +0100

    Add keyboard navigation for filter-flag input-field

--- app/components/input-field/filter-flag.js
+++ app/components/input-field/filter-flag.js
@@ -1,5 +1,6 @@
 import Component from '@glimmer/component';
 import { guidFor } from '@ember/object/internals';
+import { action } from '@ember/object';
 
 export default class InputFieldFilterFlagComponent extends Component {
   defaultOptions = [
@@ -18,5 +19,26 @@ export default class InputFieldFilterFlagComponent extends Component {
 
   get nbOfOptions() {
     return this.options.length - 1;
+  }
+
+  @action
+  selectPrevious(index) {
+    const prevIndex = index == 0 ? this.nbOfOptions : index - 1;
+    const option = this.options[prevIndex];
+    this.selectOption(option);
+  }
+
+  @action
+  selectNext(index) {
+    const nextIndex = index == this.nbOfOptions ? 0 : index + 1;
+    const option = this.options[nextIndex];
+    this.selectOption(option);
+  }
+
+  @action
+  selectOption(option) {
+    const elementId = `${this.group}-option-${option.id}`;
+    document.getElementById(elementId).focus();
+    this.args.onChange(option.value);
   }
 }
