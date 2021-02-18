c0e8842c1db84321ad14638aa1d7eed4f31a80bd
Erika Pauwels <erika.pauwels@gmail.com>
2021-02-18 14:59:37 +0100

    Refactor filter-flag input-field to Tailwind CSS

--- app/components/input-field/filter-flag.js
+++ app/components/input-field/filter-flag.js
@@ -3,16 +3,20 @@ import { guidFor } from '@ember/object/internals';
 
 export default class InputFieldFilterFlagComponent extends Component {
   defaultOptions = [
-    { label: 'n.v.t.', value: -1, id: `nvt-${guidFor(this)}` },
     { label: 'ja', value: 1, id: `yes-${guidFor(this)}` },
+    { label: 'nvt', value: -1, id: `nvt-${guidFor(this)}` },
     { label: 'nee', value: 0, id: `no-${guidFor(this)}` }
   ];
 
   get group() {
-    return this.args.group || `group-${guidFor(this)}`;
+    return this.args.group || `radiogroup-${guidFor(this)}`;
   }
 
   get options() {
     return this.args.options || this.defaultOptions;
+  }
+
+  get nbOfOptions() {
+    return this.options.length - 1;
   }
 }
