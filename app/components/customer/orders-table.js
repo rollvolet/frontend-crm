a3bd76be5e7dde140a28dff507a656c89752ea27
Erika Pauwels <erika.pauwels@gmail.com>
2021-03-20 09:02:18 +0100

    Refactor orders-table on customer detail page to Tailwind CSS

--- app/components/customer/orders-table.js
+++ app/components/customer/orders-table.js
@@ -8,47 +8,31 @@ export default class OrdersTable extends FilterComponent {
   @service router
   @service store
 
+  @tracked page = 0;
+  @tracked size = 10;
+  @tracked sort = '-offer-date';
   @tracked orders = []
 
   constructor() {
     super(...arguments);
     this.initFilter(['requestNumber', 'offerNumber', 'reference', 'name', 'postalCode', 'city', 'street']);
-    this.filter.set('page', 0);
-    this.filter.set('size', 10);
-    this.filter.set('sort', '-order-date');
-
-    // Setup observers for 2-way binded values of ember-data-table
-    this.filter.addObserver('page', this, 'onDataTableParamChange'); // eslint-disable-line ember/no-observers
-    this.filter.addObserver('size', this, 'onDataTableParamChange'); // eslint-disable-line ember/no-observers
-    this.filter.addObserver('sort', this, 'onDataTableParamChange'); // eslint-disable-line ember/no-observers
     this.search.perform(this.filter);
   }
 
   onChange(filter) {
-    if (filter.page != 0)
-      filter.set('page', 0); // search will be triggered by onDataTableParamChange()
-    else
-      this.search.perform(filter);
-  }
-
-  onDataTableParamChange() {
-    this.search.perform(this.filter);
-  }
-
-  willDestroy() {
-    this.filter.removeObserver('page', this, 'onDataTableParamChange');
-    this.filter.removeObserver('size', this, 'onDataTableParamChange');
-    this.filter.removeObserver('sort', this, 'onDataTableParamChange');
+    if (this.page != 0)
+      this.page = 0;
+    this.search.perform(filter);
   }
 
   @restartableTask
   *search(filter) {
     this.orders = yield this.store.query('order', {
       page: {
-        size: filter.size,
-        number: filter.page
+        size: this.size,
+        number: this.page
       },
-      sort: filter.sort,
+      sort: this.sort,
       include: 'building,offer',
       filter: {
         customer: {
@@ -68,8 +52,24 @@ export default class OrdersTable extends FilterComponent {
   }
 
   @action
-  clickRow(row) {
-    const orderId = row.get('id');
-    this.router.transitionTo('main.case.order.edit', this.args.customer, orderId);
+  previousPage() {
+    this.selectPage(this.page - 1);
+  }
+
+  @action
+  nextPage() {
+    this.selectPage(this.page + 1);
+  }
+
+  @action
+  selectPage(page) {
+    this.page = page;
+    this.search.perform(this.filter);
+  }
+
+  @action
+  setSort(sort) {
+    this.sort = sort;
+    this.search.perform(this.filter);
   }
 }
