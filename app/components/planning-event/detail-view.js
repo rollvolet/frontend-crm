f04c61b1b2fb0fff9741593eb09ae3399ca6afca
Erika Pauwels <erika.pauwels@gmail.com>
2021-03-29 09:44:07 +0200

    Refactoring planning-event component to Tailwind CSS

---
+++ app/components/planning-event/detail-view.js
@@ -0,0 +1,27 @@
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import { task } from 'ember-concurrency-decorators';
+
+export default class PlanningEventDetailViewComponent extends Component {
+  @service case
+
+  constructor() {
+    super(...arguments);
+    this.case.on('updateBuilding:succeeded', this, this.handleBuildingUpdatedEvent);
+  }
+
+  willDestroy() {
+    this.case.off('updateBuilding:succeeded', this, this.handleBuildingUpdatedEvent);
+    super.willDestroy(...arguments);
+  }
+
+  handleBuildingUpdatedEvent() {
+    this.args.model.reload();
+  }
+
+  @task
+  *forceUpdate() {
+    yield this.args.model.save();
+    this.editMode = false;
+  }
+}
