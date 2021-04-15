b0425afc93bf1784ef214ee3ebfa30ed4a509ed5
Erika Pauwels <erika.pauwels@gmail.com>
2021-04-15 15:58:34 +0200

    Remove old intervention detail components

--- app/components/intervention/detail-edit.js
+++
@@ -1,9 +0,0 @@
-import Component from '@glimmer/component';
-import { action } from '@ember/object';
-
-export default class InterventionDetailEditComponent extends Component {
-  @action
-  setTechnicians(employees) {
-    this.args.model.technicians = employees;
-  }
-}
