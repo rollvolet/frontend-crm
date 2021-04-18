cc4674f5b9568eeafdd94e52fa60e8635f9e7b55
Erika Pauwels <erika.pauwels@gmail.com>
2021-04-18 11:09:13 +0200

    Remove unused deposit-invoice components

--- app/components/deposit-invoice/detail-edit.js
+++
@@ -1,7 +0,0 @@
-import Component from '@glimmer/component';
-
-export default class DepositInvoiceDetailEditComponent extends Component {
-  get isLimitedUpdateOnly() {
-    return (this.args.model && this.args.model.isBooked) || this.args.invoice;
-  }
-}
--- app/components/deposit-invoice/list.js
+++
@@ -1,7 +0,0 @@
-import Component from '@glimmer/component';
-
-export default class DepositInvoiceListComponent extends Component {
-  get sortedDepositInvoices() {
-    return this.args.model.sortBy('number');
-  }
-}
