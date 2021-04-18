fd7393d7a7d8e5f911d565c08ed1ef6b9d7d8ca5
Erika Pauwels <erika.pauwels@gmail.com>
2021-04-18 15:24:00 +0200

    Delete old deposit detail components

--- app/components/deposit/table.js
+++
@@ -1,7 +0,0 @@
-import Component from '@glimmer/component';
-
-export default class DepositTableComponent extends Component {
-  get sortedDeposits() {
-    return this.args.model.sortBy('paymentDate');
-  }
-}
