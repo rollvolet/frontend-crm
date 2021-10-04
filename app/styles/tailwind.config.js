b87ea1020481447add24fada8d3a179668dab4d6
Erika Pauwels <erika.pauwels@gmail.com>
2021-10-04 16:08:53 +0200

    Refactor outstanding jobs print view to Tailwind CSS

--- app/styles/tailwind.config.js
+++ app/styles/tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
         display: ['Nunito', 'Inter var', ...defaultTheme.fontFamily.sans]
+      },
+      screens: {
+        'print': {'raw': 'print'} // => @media print { ... }
       }
     },
   },
