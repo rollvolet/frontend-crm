d3c7fb23e889288096bae8e7b37287159f87bb06
Erika Pauwels <erika.pauwels@gmail.com>
2021-10-05 12:07:49 +0200

    Refactor decoupled intervention detail page to Tailwind CSS

--- app/controllers/main/interventions/edit.js
+++ app/controllers/main/interventions/edit.js
@@ -1,9 +1,36 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { warn } from '@ember/debug';
+import { task } from 'ember-concurrency-decorators';
 
 export default class MainInterventionsEditController extends Controller {
-  @service case
+  @service case;
 
-  queryParams = ['editMode']
-  editMode = false
+  get isDisabledEdit() {
+    return this.hasInvoice || this.model.isCancelled;
+  }
+
+  get isEnabledDelete() {
+    return !this.hasInvoice && !this.model.isCancelled;
+  }
+
+  get hasInvoice() {
+    return this.case.current && this.case.current.invoice != null;
+  }
+
+  @task
+  *delete() {
+    const planningEvent = yield this.model.planningEvent;
+    try {
+      if (planningEvent && planningEvent.isPlanned) {
+        planningEvent.date = null;
+        yield planningEvent.save();
+      }
+      yield this.model.destroyRecord();
+      this.router.transitionTo('main.interventions.index');
+    } catch (e) {
+      warn(`Something went wrong while destroying intervention ${this.model.id}`, { id: 'destroy-failure' });
+      yield this.model.rollbackAttributes(); // undo delete-state
+    }
+  }
 }
