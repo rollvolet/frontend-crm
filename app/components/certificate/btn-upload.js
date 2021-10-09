2378d5facca4751708fcaa798f719deb7fb499e8
Erika Pauwels <erika.pauwels@gmail.com>
2021-10-09 14:33:40 +0200

    Remove unused certificate components

--- app/components/certificate/btn-upload.js
+++
@@ -1,8 +0,0 @@
-import Component from '@glimmer/component';
-import { guidFor } from '@ember/object/internals';
-
-export default class CertificateBtnUploadComponent extends Component {
-  get fieldName() {
-    return `certificates-${guidFor(this)}`;
-  }
-}
