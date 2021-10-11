554a36b64b8483b8f16d3a384cb97c91a718d5e1
Erika Pauwels <erika.pauwels@gmail.com>
2021-10-11 15:27:34 +0200

    Remove deprecated material design component

--- app/components/md-error-messages.js
+++
@@ -1,7 +0,0 @@
-import { tagName } from '@ember-decorators/component';
-import Component from '@ember/component';
-
-@tagName('')
-export default class MdErrorMessages extends Component {
-  errors = null;
-}
