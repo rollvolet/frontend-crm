55695e1f58ef70b241e378b0c9fcf7b0dd9eeb12
Erika Pauwels <erika.pauwels@gmail.com>
2021-10-11 16:54:51 +0200

    Add inline edit on contact/building details of a case

---
+++ app/components/case/contact-panel.js
@@ -0,0 +1,31 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { later } from '@ember/runloop';
+import { keepLatestTask } from 'ember-concurrency-decorators';
+
+export default class CaseContactPanelComponent extends Component {
+  @tracked isOpenEditModal = false;
+  @tracked showModalContent = false;
+
+  @keepLatestTask
+  *save() {
+    const { validations } = yield this.args.model.validate();
+    if (validations.isValid)
+      yield this.args.model.save();
+  }
+
+  @action
+  openEditModal() {
+    this.isOpenEditModal = true;
+    this.showModalContent = true;
+  }
+
+  @action
+  closeEditModal() {
+    this.showModalContent = false;
+    later(this, function() {
+      this.isOpenEditModal = false;
+    }, 200); // delay to finish leave CSS animation
+  }
+}
