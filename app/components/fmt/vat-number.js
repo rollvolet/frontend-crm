30db9444e7672f7c4a959ceb9a8e0f3d7d3fb1c1
Erika Pauwels <erika.pauwels@gmail.com>
2021-12-03 14:05:59 +0100

    Move all formatting components to subfolder

--- app/components/fmt/vat-number.js
+++ app/components/fmt/vat-number.js
@@ -1,5 +1,5 @@
 import Component from '@glimmer/component';
-import formatVatNumber from '../utils/format-vat-number';
+import formatVatNumber from '../../utils/format-vat-number';
 
 export default class FmtVatNumberComponent extends Component {
   get formattedValue() {
