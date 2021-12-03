11ea9e3d48420c21092d2c4dec826c9a40e4da8d
Erika Pauwels <erika.pauwels@gmail.com>
2021-12-03 15:39:18 +0100

    Add formatting for AD-number
    Group per 3 characters

---
+++ app/helpers/format-request-number.js
@@ -0,0 +1,14 @@
+import { helper } from '@ember/component/helper';
+
+const groupPer3Chars = /(?=(?:...)*$)/;
+
+export function formatRequestNumber([number]) {
+  if (number) {
+    // number may be a string or number
+    return `${number}`.split(groupPer3Chars).join(' ');
+  } else {
+    return number;
+  }
+}
+
+export default helper(formatRequestNumber);
