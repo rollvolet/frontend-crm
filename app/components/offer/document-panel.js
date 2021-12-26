9b4a8605d324e43beb90625b1e06da67c8d7d2a5
Erika Pauwels <erika.pauwels@gmail.com>
2021-12-26 19:10:49 +0100

    Make offer document version number editable

--- app/components/offer/document-panel.js
+++ app/components/offer/document-panel.js
@@ -1,5 +1,6 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 import { warn } from '@ember/debug';
 import { task, all } from 'ember-concurrency';
@@ -7,6 +8,8 @@ import { task, all } from 'ember-concurrency';
 export default class OfferDocumentPanelComponent extends Component {
   @service documentGeneration;
   @service store;
+
+  @tracked versionEditMode = false;
 
   get sortedOfferlines() {
     return this.args.model.offerlines.sortBy('sequenceNumber');
@@ -84,8 +87,28 @@ export default class OfferDocumentPanelComponent extends Component {
     }
   }
 
+  @task
+  *saveDocumentVersion() {
+    if (this.args.model.hasDirtyAttributes) {
+      const { validations } = yield this.args.model.validate();
+      if (validations.isValid) {
+        yield this.args.model.save();
+      }
+    }
+  }
+
   @action
   downloadOfferDocument() {
     this.documentGeneration.downloadOfferDocument(this.args.model);
+  }
+
+  @action
+  openVersionEdit() {
+    this.versionEditMode = true;
+  }
+
+  @action
+  closeVersionEdit() {
+    this.versionEditMode = false;
   }
 }
