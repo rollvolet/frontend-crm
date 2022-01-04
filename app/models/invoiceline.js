10dde105e54763072e8172212b3948af5e7d166c
Erika Pauwels <erika.pauwels@gmail.com>
2022-01-04 19:30:13 +0100

    Add currency to invoiceline

--- app/models/invoiceline.js
+++ app/models/invoiceline.js
@@ -3,12 +3,18 @@ import { validator, buildValidations } from 'ember-cp-validations';
 
 const Validations = buildValidations({
   amount: validator('presence', true),
+  currency: validator('presence', true),
   vatRate: validator('presence', true),
   description: validator('presence', true),
 });
 
 export default class InvoicelineModel extends Model.extend(Validations) {
   @attr sequenceNumber;
+  @attr('string', {
+    defaultValue() {
+      return 'EUR';
+    },
+  }) currency;
   @attr description;
   @attr('number') amount;
   @attr order;
