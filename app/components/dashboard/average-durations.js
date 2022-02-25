ae6aba4e3c77aeeafe90fd4873a6ac0a9246485a
Erika Pauwels <erika.pauwels@gmail.com>
2022-02-25 17:59:47 +0100

    Add average duration statistics to dashboard

---
+++ app/components/dashboard/average-durations.js
@@ -0,0 +1,33 @@
+import Component from '@glimmer/component';
+import fetch, { Headers } from 'fetch';
+import { keepLatestTask } from 'ember-concurrency';
+import { tracked } from '@glimmer/tracking';
+
+const reportApiPath = '/api/reports/average-duration-report';
+
+export default class DashboardOutstandingJobsComponent extends Component {
+  @tracked averagePlacementDuration;
+  @tracked averageInterventionDuration;
+
+  constructor() {
+    super(...arguments);
+    this.loadData.perform();
+  }
+
+  @keepLatestTask
+  *loadData() {
+    const endpoint = new URL(reportApiPath, window.location.origin);
+
+    const response = yield fetch(endpoint, {
+      headers: new Headers({
+        Accept: 'application/json',
+      }),
+    });
+    const json = yield response.json();
+    if (json.data) {
+      const report = json.data.attributes;
+      this.averagePlacementDuration = report['average-placement-duration'];
+      this.averageInterventionDuration = report['average-intervention-duration'];
+    }
+  }
+}
