7030f9e3fbe10ac50da80af4b74388ea2781e583
Erika Pauwels <erika.pauwels@gmail.com>
2023-11-17 15:22:56 +0100

    Calculate average durations on dashboard based on mu-search

--- app/components/dashboard/average-durations.js
+++ app/components/dashboard/average-durations.js
@@ -5,6 +5,7 @@ import { tracked } from '@glimmer/tracking';
 import differenceInDays from 'date-fns/differenceInDays';
 import config from '../../config';
 import constants from '../../config/constants';
+import search from '../../utils/mu-search';
 import median from '../../utils/math/median';
 
 const { PAGE_SIZE } = config;
@@ -28,47 +29,37 @@ export default class DashboardOutstandingJobsComponent extends Component {
 
   @keepLatestTask
   *calculateAveragePlacementDuration() {
-    const invoices = yield this.store.query('invoice', {
-      page: {
-        number: 0,
-        size: PAGE_SIZE.AVERAGE_NUMBER_REPORTS,
-      },
-      sort: '-invoice-date',
-      include: 'case.order',
-      'filter[case][delivery-method][:uri:]': DELIVERY_METHODS.TO_BE_INSTALLED,
-      'filter[case][:has:order]': true,
+    const orders = yield search('orders', 0, PAGE_SIZE.AVERAGE_NUMBER_REPORTS, '-invoice-date', {
+      deliveryMethodUri: DELIVERY_METHODS.TO_BE_INSTALLED,
+      ':has:invoiceId': 't',
     });
 
-    const dayDiffs = yield Promise.all(
-      invoices.toArray().map(async (invoice) => {
-        const _case = await invoice.case;
-        const order = await _case.order;
-        return differenceInDays(invoice.invoiceDate, order.orderDate);
-      })
-    );
+    const dayDiffs = orders.toArray().map((order) => {
+      const invoiceDate = new Date(Date.parse(order.invoiceDate));
+      const orderDate = new Date(Date.parse(order.orderDate));
+      return differenceInDays(invoiceDate, orderDate);
+    });
 
     this.averagePlacementDuration = median(dayDiffs);
   }
 
   @keepLatestTask
   *calculateAverageInterventionDuration() {
-    const invoices = yield this.store.query('invoice', {
-      page: {
-        number: 0,
-        size: PAGE_SIZE.AVERAGE_NUMBER_REPORTS,
-      },
-      sort: '-invoice-date',
-      include: 'case.intervention',
-      'filter[case][:has:intervention]': true,
-    });
-
-    const dayDiffs = yield Promise.all(
-      invoices.toArray().map(async (invoice) => {
-        const _case = await invoice.case;
-        const intervention = await _case.intervention;
-        return differenceInDays(invoice.invoiceDate, intervention.interventionDate);
-      })
+    const interventions = yield search(
+      'interventions',
+      0,
+      PAGE_SIZE.AVERAGE_NUMBER_REPORTS,
+      '-invoice-date',
+      {
+        ':has:invoiceId': 't',
+      }
     );
+
+    const dayDiffs = interventions.toArray().map((intervention) => {
+      const invoiceDate = new Date(Date.parse(intervention.invoiceDate));
+      const interventionDate = new Date(Date.parse(intervention.interventionDate));
+      return differenceInDays(invoiceDate, interventionDate);
+    });
 
     this.averageInterventionDuration = median(dayDiffs);
   }
