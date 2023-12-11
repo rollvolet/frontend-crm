692fea867af4b0ecaa6d47bb82e0c29007de26e5
Erika Pauwels <erika.pauwels@gmail.com>
2023-12-11 21:52:02 +0100

    Prevent creation of order if delivery method is missing

--- app/components/case/tabs/order.js
+++ app/components/case/tabs/order.js
@@ -1,6 +1,13 @@
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class CaseTabsOrderComponent extends Component {
+  @service router;
+
+  @tracked isOpenMissingDeliveryMethodModal = false;
+
   get isNextStep() {
     return this.args.currentStep == 'offer';
   }
@@ -12,5 +19,21 @@ export default class CaseTabsOrderComponent extends Component {
       !this.args.model.offer.get('isMasteredByAccess') &&
       this.args.model.order.get('id') == null
     );
+  }
+
+  @action
+  async navigateToOrderRoute() {
+    const deliveryMethod = await this.args.model.deliveryMethod;
+    if (deliveryMethod) {
+      const offer = await this.args.model.offer;
+      this.router.transitionTo('main.case.offer.edit.order', this.args.model.id, offer.id);
+    } else {
+      this.isOpenMissingDeliveryMethodModal = true;
+    }
+  }
+
+  @action
+  closeMissingDeliveryMethodModal() {
+    this.isOpenMissingDeliveryMethodModal = false;
   }
 }
