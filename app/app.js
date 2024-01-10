d6abfa273676d84af242e378ba701045776ba04c
Erika Pauwels <erika.pauwels@gmail.com>
2024-01-10 15:38:57 +0100

    Bump ember-flatpickr

--- app/app.js
+++ app/app.js
@@ -2,6 +2,10 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'rollvolet-crm/config/environment';
+import flatpickr from 'flatpickr';
+import lang from 'flatpickr/dist/l10n';
+
+flatpickr.localize(lang.nl);
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
