c02693e30870e4e38689a98677108e96a0fe30d2
Erika Pauwels <erika.pauwels@gmail.com>
2024-01-18 13:20:22 +0100

    Fix refreshing model of accountancy exports list
    For quick exports we must add some dummy waiting time to ensure the
    resource cache is invalidated in the backend.
    
    Use router.refresh instead of sending refreshModel to route.

--- app/routes/main/accountancy/exports.js
+++ app/routes/main/accountancy/exports.js
@@ -1,11 +1,5 @@
 import DataTableRoute from '../../../utils/data-table-route';
-import { action } from '@ember/object';
 
 export default class MainAccountancyExportsRoute extends DataTableRoute {
   modelName = 'accountancy-export';
-
-  @action
-  refreshModel() {
-    this.refresh();
-  }
 }
