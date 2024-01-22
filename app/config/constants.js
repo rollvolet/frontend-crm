c3a0b9890ae6c54c20d1b71232b66fb62f4e3b98
Erika Pauwels <erika.pauwels@gmail.com>
2024-01-22 09:11:08 +0100

    Fix URI of customer class

--- app/config/constants.js
+++ app/config/constants.js
@@ -76,7 +76,7 @@ export default {
     INTERVENTION: 'http://data.rollvolet.be/vocabularies/crm/Intervention',
     OFFER: 'http://schema.org/Offer',
     INVOICE_DOCUMENT: 'https://purl.org/p2p-o/document#E-Invoice',
-    CUSTOMER: 'http://www.w3.org/2006/vcard/ns#Vcard',
+    CUSTOMER: 'http://www.w3.org/2006/vcard/ns#VCard',
     CONTACT: 'http://www.semanticdesktop.org/ontologies/2007/03/22/nco#Contact',
     BUILDING: 'https://data.vlaanderen.be/ns/gebouw#Gebouw',
   },
