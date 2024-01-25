168f781ba53f289d44af12e1bebd9c3f1597447f
Erika Pauwels <erika.pauwels@gmail.com>
2024-01-25 16:11:25 +0100

    Display vat rate name instead of rate in case header

--- app/models/vat-rate.js
+++ app/models/vat-rate.js
@@ -2,7 +2,6 @@ import Model, { attr, hasMany } from '@ember-data/model';
 
 export default class VatRateModel extends Model {
   @attr('string') uri;
-  @attr('string') code;
   @attr('string') name;
   @attr('string') rate;
   @attr('number') position;
