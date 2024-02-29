70abfb1128703eb0d903da9ed96c59646b60d1c4
Erika Pauwels <erika.pauwels@gmail.com>
2024-02-29 17:27:59 +0100

    Use async data to load case tabs data

--- app/components/case/tabs/offer.js
+++ app/components/case/tabs/offer.js
@@ -1,16 +1,42 @@
 import Component from '@glimmer/component';
+import { cached } from '@glimmer/tracking';
+import { TrackedAsyncData } from 'ember-async-data';
 
 export default class CaseTabsOfferComponent extends Component {
   get isNextStep() {
     return this.args.currentStep == 'request';
   }
 
+  @cached
+  get customer() {
+    return new TrackedAsyncData(this.args.model.customer);
+  }
+
+  get hasLinkedCustomer() {
+    return this.customer.isResolved && this.customer.value != null;
+  }
+
+  @cached
+  get request() {
+    return new TrackedAsyncData(this.args.model.request);
+  }
+
+  get hasRequest() {
+    return this.request.isResolved && this.request.value != null;
+  }
+
+  @cached
+  get offer() {
+    return new TrackedAsyncData(this.args.model.offer);
+  }
+
+  get hasOffer() {
+    return this.offer.isResolved && this.offer.value != null;
+  }
+
   get canCreateNew() {
     return (
-      !this.args.model.isCancelled &&
-      this.args.model.customer.get('id') &&
-      this.args.model.request.get('id') &&
-      this.args.model.offer.get('id') == null
+      !this.args.model.isCancelled && this.hasLinkedCustomer && this.hasRequest && !this.hasOffer
     );
   }
 }
--- app/components/case/tabs/order.js
+++ app/components/case/tabs/order.js
@@ -1,6 +1,7 @@
 import Component from '@glimmer/component';
 import { service } from '@ember/service';
-import { tracked } from '@glimmer/tracking';
+import { tracked, cached } from '@glimmer/tracking';
+import { TrackedAsyncData } from 'ember-async-data';
 import { action } from '@ember/object';
 
 export default class CaseTabsOrderComponent extends Component {
@@ -12,12 +13,39 @@ export default class CaseTabsOrderComponent extends Component {
     return this.args.currentStep == 'offer';
   }
 
+  @cached
+  get customer() {
+    return new TrackedAsyncData(this.args.model.customer);
+  }
+
+  get hasLinkedCustomer() {
+    return this.customer.isResolved && this.customer.value != null;
+  }
+
+  @cached
+  get offer() {
+    return new TrackedAsyncData(this.args.model.offer);
+  }
+
+  get hasOffer() {
+    return this.offer.isResolved && this.offer.value != null;
+  }
+
+  @cached
+  get order() {
+    return new TrackedAsyncData(this.args.model.order);
+  }
+
+  get hasOrder() {
+    return this.order.isResolved && this.order.value != null;
+  }
+
   get canCreateNew() {
     return (
       !this.args.model.isCancelled &&
-      this.args.model.offer.get('id') &&
-      !this.args.model.offer.get('isMasteredByAccess') &&
-      this.args.model.order.get('id') == null
+      this.hasOffer &&
+      !this.offer.value.isMasteredByAccess &&
+      !this.hasOrder
     );
   }
 
