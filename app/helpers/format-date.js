645aafab1a575e5d147ff9a6c1054fb11379ebeb
Erika Pauwels <erika.pauwels@gmail.com>
2024-05-11 17:08:48 +0200

    Make date-format helper accept array of dates
    Logs a warning but doesn't make the app crash

--- app/helpers/format-date.js
+++ app/helpers/format-date.js
@@ -1,4 +1,5 @@
 import { helper } from '@ember/component/helper';
+import { warn } from '@ember/debug';
 import formatFn from 'date-fns/format';
 import parseISO from 'date-fns/parseISO';
 import { nlBE } from 'date-fns/locale';
@@ -6,6 +7,13 @@ import { nlBE } from 'date-fns/locale';
 export function formatDate([date, format, customOptions = {}]) {
   const options = Object.assign({ locale: nlBE }, customOptions);
   if (date) {
+    if (Array.isArray(date)) {
+      warn('format-date helper exepected a single date, but got an array as value to format', {
+        id: 'invalidate-date',
+      });
+      date = date[0];
+    }
+
     if (typeof date == 'string') {
       return formatFn(parseISO(date), format, options);
     } else {
