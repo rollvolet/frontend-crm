17c4990a2a26f1dc2936e8f6929b86d6e2ffa63f
Erika Pauwels <erika.pauwels@gmail.com>
2024-05-21 19:05:15 +0200

    Fix update of requires visit flag on request

--- app/components/time-slot/detail-view.js
+++ app/components/time-slot/detail-view.js
@@ -1,8 +1,33 @@
 import Component from '@glimmer/component';
-import { isPresent } from '@ember/utils';
+import { isBlank } from '@ember/utils';
+import isPast from 'date-fns/isPast';
 
 export default class TimeSlotDetailViewComponent extends Component {
-  get isScheduled() {
-    return isPresent(this.args.model?.id);
+  get isUnscheduled() {
+    return isBlank(this.args.model?.id);
+  }
+
+  get isOverdue() {
+    return this.isUnscheduled && isPast(this.args.date);
+  }
+
+  get borderColor() {
+    if (this.isOverdue) {
+      return 'border-red-400';
+    } else if (this.isUnscheduled) {
+      return 'border-gray-400';
+    } else {
+      return null;
+    }
+  }
+
+  get bgColor() {
+    if (this.isOverdue) {
+      return 'bg-red-400';
+    } else if (this.isUnscheduled) {
+      return 'bg-gray-400';
+    } else {
+      return null;
+    }
   }
 }
