9fad3f3520c48892532365e573cff1c1c82a74d5
Erika Pauwels <erika.pauwels@gmail.com>
2024-06-03 14:43:34 +0200

    Use format-customer-name helper instead of component

--- app/helpers/format-customer-name.js
+++ app/helpers/format-customer-name.js
@@ -4,18 +4,20 @@ export default function formatCustomerName(customer, opts) {
   const options = Object.assign({ printAll: false }, opts);
 
   const parts = [];
-  if (customer.printInFront) {
-    parts.push(customer.honorificPrefix);
-  }
-  if (customer.printPrefix || options.printAll) {
-    parts.push(customer.prefix);
-  }
-  parts.push(customer.name);
-  if (customer.printSuffix || options.printAll) {
-    parts.push(customer.suffix);
-  }
-  if (!customer.printInFront) {
-    parts.push(customer.honorificPrefix);
+  if (customer) {
+    if (customer.printInFront) {
+      parts.push(customer.honorificPrefix);
+    }
+    if (customer.printPrefix || options.printAll) {
+      parts.push(customer.prefix);
+    }
+    parts.push(customer.name);
+    if (customer.printSuffix || options.printAll) {
+      parts.push(customer.suffix);
+    }
+    if (!customer.printInFront) {
+      parts.push(customer.honorificPrefix);
+    }
   }
 
   return parts.filter((e) => isPresent(e?.trim())).join(' ');
