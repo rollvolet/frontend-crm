71f144a4b57925782b6b8d48ce5d279d27901d82
Erika Pauwels <erika.pauwels@gmail.com>
2024-06-12 09:33:36 +0200

    Remove button to delete intervention without invoice

--- app/components/intervention/panels.js
+++ app/components/intervention/panels.js
@@ -2,7 +2,6 @@ import Component from '@glimmer/component';
 import { cached } from '@glimmer/tracking';
 import { TrackedAsyncData } from 'ember-async-data';
 import { service } from '@ember/service';
-import { task } from 'ember-concurrency';
 
 export default class InterventionPanelsComponent extends Component {
   @service router;
@@ -27,34 +26,5 @@ export default class InterventionPanelsComponent extends Component {
 
   get isDisabledEdit() {
     return (this.case.isResolved && this.case.value.isCancelled) || this.hasInvoice;
-  }
-
-  get isEnabledDelete() {
-    return (
-      this.case.isResolved &&
-      this.case.value.isOngoing &&
-      this.invoice?.isResolved &&
-      this.invoice.value == null
-    );
-  }
-
-  @task
-  *delete() {
-    const _case = yield this.args.model.case;
-    const customer = yield _case.customer;
-
-    const visit = yield this.args.model.visit;
-    if (visit) {
-      yield visit.destroyRecord();
-    }
-
-    yield this.args.model.destroyRecord();
-    yield _case.destroyRecord();
-
-    if (customer) {
-      this.router.transitionTo('main.customers.edit.index', customer.id);
-    } else {
-      this.router.transitionTo('main.interventions.index');
-    }
   }
 }
